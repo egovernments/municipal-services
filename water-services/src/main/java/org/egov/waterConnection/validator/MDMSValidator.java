895cd0690d8119ecb901bb2190fcad965732cca5
shriya-egov-tarento <shriya.bajpai@egovernments.org>
2019-10-31 13:47:44 +0530

    mdms validator added and modified

--- water-services/src/main/java/org/egov/waterConnection/validator/MDMSValidator.java
+++ water-services/src/main/java/org/egov/waterConnection/validator/MDMSValidator.java
@@ -37,6 +37,8 @@ public class MDMSValidator {
 	private String mdmsEndpoint;
 
 	public void validateMasterData(WaterConnectionRequest request) {
+		
+		 String jsonpath=WCConstants.JSONPATH_ROOT+WCConstants.JSONPATH_CODE_CONNECTION_CATEGORY + WCConstants.JSONPATH_CODE_CONNECTION_TYPE+WCConstants.JSONPATH_CODE_WATER_SOURCE;
 		Map<String, String> errorMap = new HashMap<>();
 		String tenantId = request.getWaterConnection().getProperty().getTenantId();
 
@@ -44,87 +46,16 @@ public class MDMSValidator {
 				WCConstants.MDMS_WC_Water_Source };
 		List<String> names = new ArrayList<>(Arrays.asList(masterNames));
 		Map<String, List<String>> codes = getAttributeValues(tenantId, WCConstants.MDMS_WC_MOD_NAME, names, "$.*.code",
-				WCConstants.JSONPATH_CODES, request.getRequestInfo());
+				jsonpath, request.getRequestInfo());
 		 validateMDMSData(masterNames, codes);
-		 validateCodes(request.getWaterConnection(),codes,errorMap);
+	//	 validateCodes(request.getWaterConnection(),codes,errorMap);
 
 		if (!errorMap.isEmpty())
 			throw new CustomException(errorMap);
 	}
 	
-	
-	   private static Map<String,String> validateCodes(WaterConnection waterConnection,Map<String,List<String>> codes,Map<String,String> errorMap){
-	        log.info("Validating WaterConnection");
-//	        properties.forEach(property -> {
-//	            property.getPropertyDetails().forEach(propertyDetail -> {
-//
-//	                if(!codes.get(PTConstants.MDMS_PT_PROPERTYTYPE).contains(propertyDetail.getPropertyType()) && propertyDetail.getPropertyType()!=null){
-//	                    errorMap.put("Invalid PROPERTYTYPE","The PropertyType '"+propertyDetail.getPropertyType()+"' does not exists");
-//	                }
-//
-//	                if(!codes.get(PTConstants.MDMS_PT_SUBOWNERSHIP).contains(propertyDetail.getSubOwnershipCategory()) && propertyDetail.getSubOwnershipCategory()!=null){
-//	                    errorMap.put("Invalid SUBOWNERSHIPCATEGORY","The SubOwnershipCategory '"+propertyDetail.getSubOwnershipCategory()+"' does not exists");
-//	                }
-//
-//	                if(!codes.get(PTConstants.MDMS_PT_OWNERSHIP).contains(propertyDetail.getOwnershipCategory()) && propertyDetail.getOwnershipCategory()!=null){
-//	                    errorMap.put("Invalid OWNERSHIPCATEGORY","The OwnershipCategory '"+propertyDetail.getOwnershipCategory()+"' does not exists");
-//	                }
-//
-//	                if(!codes.get(PTConstants.MDMS_PT_PROPERTYSUBTYPE).contains(propertyDetail.getPropertySubType()) && propertyDetail.getPropertySubType()!=null){
-//	                    errorMap.put(ErrorConstants.INVALID_PROPERTYSUBTYPE,"The PropertySubType '"+propertyDetail.getPropertySubType()+"' does not exists");
-//	                }
-//
-//	                if(!codes.get(PTConstants.MDMS_PT_USAGEMAJOR).contains(propertyDetail.getUsageCategoryMajor()) && propertyDetail.getUsageCategoryMajor()!=null){
-//	                    errorMap.put("INVALID USAGECATEGORYMAJOR","The UsageCategoryMajor '"+propertyDetail.getUsageCategoryMajor()+"' at Property level does not exists");
-//	                }
-//
-//	                if(!CollectionUtils.isEmpty(propertyDetail.getUnits()))
-//	                    propertyDetail.getUnits().forEach(unit ->{
-//	                        if(!codes.get(PTConstants.MDMS_PT_USAGEMAJOR).contains(unit.getUsageCategoryMajor()) && unit.getUsageCategoryMajor()!=null){
-//	                            errorMap.put("INVALID USAGECATEGORYMAJOR","The UsageCategoryMajor '"+unit.getUsageCategoryMajor()+"' at unit level does not exists");
-//	                        }
-//
-//	                        if(!codes.get(PTConstants.MDMS_PT_USAGEMINOR).contains(unit.getUsageCategoryMinor()) && unit.getUsageCategoryMinor()!=null){
-//	                            errorMap.put("INVALID USAGECATEGORYMINOR","The UsageCategoryMinor '"+unit.getUsageCategoryMinor()+"' does not exists");
-//	                        }
-//
-//	                        if(!codes.get(PTConstants.MDMS_PT_USAGESUBMINOR).contains(unit.getUsageCategorySubMinor()) && unit.getUsageCategorySubMinor()!=null){
-//	                            errorMap.put("INVALID USAGECATEGORYSUBMINOR","The UsageCategorySubMinor '"+unit.getUsageCategorySubMinor()+"' does not exists");
-//	                        }
-//
-//	                        if(!codes.get(PTConstants.MDMS_PT_USAGEDETAIL).contains(unit.getUsageCategoryDetail()) && unit.getUsageCategoryDetail()!=null){
-//	                            errorMap.put("INVALID USAGECATEGORYDETAIL","The UsageCategoryDetail "+unit.getUsageCategoryDetail()+" does not exists");
-//	                        }
-//
-//	                        if(!codes.get(PTConstants.MDMS_PT_CONSTRUCTIONTYPE).contains(unit.getConstructionType()) && unit.getConstructionType()!=null){
-//	                            errorMap.put("INVALID CONSTRUCTIONTYPE","The ConstructionType '"+unit.getConstructionType()+"' does not exists");
-//	                        }
-//
-//	                        if(!codes.get(PTConstants.MDMS_PT_CONSTRUCTIONSUBTYPE).contains(unit.getConstructionSubType()) && unit.getConstructionSubType()!=null){
-//	                            errorMap.put("INVALID CONSTRUCTIONSUBTYPE","The ConstructionSubType '"+unit.getConstructionSubType()+"' does not exists");
-//	                        }
-//
-//	                        if(!codes.get(PTConstants.MDMS_PT_OCCUPANCYTYPE).contains(unit.getOccupancyType()) && unit.getOccupancyType()!=null){
-//	                            errorMap.put("INVALID OCCUPANCYTYPE","The OccupancyType '"+unit.getOccupancyType()+"' does not exists");
-//	                        }
-//
-//	                        if("RENTED".equalsIgnoreCase(unit.getOccupancyType())){
-//	                            if(unit.getArv()==null || unit.getArv().compareTo(new BigDecimal(0))!=1)
-//	                                errorMap.put("INVALID ARV","Total Annual Rent should be greater than zero ");
-//	                        }
-//	                    });
-//
-//	                propertyDetail.getOwners().forEach(owner ->{
-//	                    if(!codes.get(PTConstants.MDMS_PT_OWNERTYPE).contains(owner.getOwnerType()) && owner.getOwnerType()!=null){
-//	                        errorMap.put("INVALID OWNERTYPE","The OwnerType '"+owner.getOwnerType()+"' does not exists");
-//	                    }
-//	                });
-//	            });
-//
-//	        });
-	        return errorMap;
 
-	    }
+	    
 
 	private Map<String, List<String>> getAttributeValues(String tenantId, String moduleName, List<String> names,
 			String filter, String jsonpath, RequestInfo requestInfo) {
