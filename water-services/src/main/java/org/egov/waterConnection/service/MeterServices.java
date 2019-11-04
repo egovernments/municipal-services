eee4d8f862d014a2df2a13f3104fba67cfc55e9f
Amit-yadav-egov-tarento <amit.yadav@egovernments.org>
2019-11-04 12:40:13 +0530

    Meter Water Reading controller

--- water-services/src/main/java/org/egov/waterConnection/service/MeterServices.java
+++ water-services/src/main/java/org/egov/waterConnection/service/MeterServices.java
@@ -1,5 +1,10 @@
 package org.egov.waterConnection.service;
 
-public interface MeterServices {
+import java.util.List;
 
+import org.egov.waterConnection.model.MeterConnectionRequest;
+import org.egov.waterConnection.model.MeterReading;
+
+public interface MeterServices {
+	public List<MeterReading> addMeterReading(MeterConnectionRequest meterConnectionRequest);
 }
--- water-services/src/main/java/org/egov/waterConnection/service/MeterServicesImpl.java
+++ water-services/src/main/java/org/egov/waterConnection/service/MeterServicesImpl.java
@@ -1,5 +1,17 @@
 package org.egov.waterConnection.service;
 
+import java.util.List;
+
+import org.egov.waterConnection.model.MeterConnectionRequest;
+import org.egov.waterConnection.model.MeterReading;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MeterServicesImpl implements MeterServices {
+
+	@Override
+	public List<MeterReading> addMeterReading(MeterConnectionRequest meterConnectionRequest) {
+		return null;
+	}
 
 }
