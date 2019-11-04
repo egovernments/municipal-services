4cf62a1cd7a37847b4545d689371a1681d810de4
Amit-yadav-egov-tarento <amit.yadav@egovernments.org>
2019-11-04 18:12:37 +0530

    Water Calculation Microservice Added

---
+++ ws-calculation/src/main/java/org/egov/wscalculation/WsCalculationApplication.java
@@ -0,0 +1,13 @@
+package org.egov.wscalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsCalculationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsCalculationApplication.class, args);
+	}
+
+}
