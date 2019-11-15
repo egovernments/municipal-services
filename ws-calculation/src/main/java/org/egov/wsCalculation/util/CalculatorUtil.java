12a98a36bc8dea04a64c9283c5c4f8527d669ec6
shriya-egov-tarento <shriya.bajpai@egovernments.org>
2019-11-15 11:52:45 +0530

    financial master data method added for ws calculation

--- ws-calculation/src/main/java/org/egov/wsCalculation/util/CalculatorUtil.java
+++ ws-calculation/src/main/java/org/egov/wsCalculation/util/CalculatorUtil.java
@@ -1,14 +1,19 @@
 package org.egov.wsCalculation.util;
 
+import static org.egov.pt.calculator.util.CalculatorConstants.SERVICE_FIELD_VALUE_PT;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.mdms.model.MasterDetail;
 import org.egov.mdms.model.MdmsCriteria;
 import org.egov.mdms.model.MdmsCriteriaReq;
 import org.egov.mdms.model.ModuleDetail;
+import org.egov.pt.calculator.util.CalculatorConstants;
 import org.egov.wscalculation.config.WSCalculationConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -44,6 +49,27 @@ public class CalculatorUtil {
 	 */
 	public StringBuilder getMdmsSearchUrl() {
 		return new StringBuilder().append(calculationConfig.getMdmsHost()).append(calculationConfig.getMdmsEndPoint());
+	}
+	
+	/**
+	 * Prepares and returns Mdms search request with financial master criteria
+	 *
+	 * @param requestInfo
+	 * @param assesmentYears
+	 * @return
+	 */
+	public MdmsCriteriaReq getFinancialYearRequest(RequestInfo requestInfo, Set<String> assesmentYears, String tenantId) {
+
+		String assessmentYearStr = StringUtils.join(assesmentYears,",");
+		MasterDetail mstrDetail = MasterDetail.builder().name(CalculatorConstants.FINANCIAL_YEAR_MASTER)
+				.filter("[?(@." + CalculatorConstants.FINANCIAL_YEAR_RANGE_FEILD_NAME + " IN [" + assessmentYearStr + "]" +
+						" && @.module== '"+SERVICE_FIELD_VALUE_PT+"')]")
+				.build();
+		ModuleDetail moduleDetail = ModuleDetail.builder().moduleName(CalculatorConstants.FINANCIAL_MODULE)
+				.masterDetails(Arrays.asList(mstrDetail)).build();
+		MdmsCriteria mdmsCriteria = MdmsCriteria.builder().moduleDetails(Arrays.asList(moduleDetail)).tenantId(tenantId)
+				.build();
+		return MdmsCriteriaReq.builder().requestInfo(requestInfo).mdmsCriteria(mdmsCriteria).build();
 	}
 
 }
