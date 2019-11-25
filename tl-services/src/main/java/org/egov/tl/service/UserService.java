3fb6d3d9f328677c86d48d67e561a01833cfc7d2
gopesh <gopesh.yadav@egovernments.org>
2019-11-26 00:25:11 +0530

    bpa search based on mob to populate uuid

--- tl-services/src/main/java/org/egov/tl/service/UserService.java
+++ tl-services/src/main/java/org/egov/tl/service/UserService.java
@@ -78,6 +78,19 @@ public class UserService{
 
             tradeLicense.getTradeLicenseDetail().getOwners().forEach(owner ->
             {
+                String businessService = tradeLicense.getBusinessService();
+                if (businessService == null)
+                    businessService = businessService_TL;
+                switch (businessService) {
+                    case businessService_BPA:
+                        if (owner.getUuid() == null) {
+                            UserDetailResponse userDetailResponse = getUser(TradeLicenseSearchCriteria.builder().mobileNumber(owner.getMobileNumber()).tenantId(tradeLicense.getTenantId()).build(), request.getRequestInfo());
+                            if (!userDetailResponse.getUser().isEmpty()) {
+                                owner.setUuid(userDetailResponse.getUser().get(0).getUuid());
+                            }
+                        }
+                        break;
+                }
                 if(owner.getUuid()==null)
                     {
                         addUserDefaultFields(tradeLicense.getTenantId(),role,owner);
