5c311818870e457715f8f251782270d10b6cea31
Karan Bajrang Bishnoi <karanbk.bpgc@gmail.com>
2019-11-28 14:09:25 +0530

    fireNoc old repo copy

---
+++ firenoc-services/src/api/index.js
@@ -0,0 +1,19 @@
+import { version } from "../../package.json";
+import { Router } from "express";
+import create from "./create";
+import search from "./search";
+import update from "./update";
+
+export default ({ config, db }) => {
+  let api = Router();
+
+  api.use("/firenoc-services/v1", create({ config, db }));
+  api.use("/firenoc-services/v1", search({ config, db }));
+  api.use("/firenoc-services/v1", update({ config, db }));
+  // perhaps expose some API metadata at the root
+  api.get("/", (req, res) => {
+    res.json({ version });
+  });
+
+  return api;
+};
