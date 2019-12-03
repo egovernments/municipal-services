aa69c195d54574e843c370a626cb47b85863f333
kaviyarasan1993 <kaviyarasanit@gmail.com>
2019-12-03 15:03:01 +0530

    property registry renamed

---
+++ property-services/src/main/java/org/egov/pt/models/Unit.java
@@ -0,0 +1,68 @@
+package org.egov.pt.models;
+
+import javax.validation.constraints.NotNull;
+
+import org.egov.pt.models.enums.OccupancyType;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Unit
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Unit   {
+	
+        @JsonProperty("id")
+        private String id;
+        
+        @JsonProperty("assessmentId")
+        private String assessmentId;
+
+        @JsonProperty("tenantId")
+        @NotNull
+        private String tenantId;
+
+        @JsonProperty("floorNo")
+        private String floorNo;
+
+        @JsonProperty("unitArea")
+        @NotNull
+        private Double unitArea;
+
+        @JsonProperty("usageCategory")
+        @NotNull
+        private String usageCategory;
+
+        @JsonProperty("occupancyType")
+        @NotNull
+        private OccupancyType occupancyType;
+
+        @JsonProperty("occupancyDate")
+        @NotNull
+        private Long occupancyDate;
+
+        @JsonProperty("constructionType")
+        @NotNull
+        private String constructionType;
+        
+        @JsonProperty("active")
+        private Boolean active;
+
+        @JsonProperty("arv")
+        private Double arv;
+        
+        @JsonProperty("auditDetails")
+        private AuditDetails auditDetails;
+
+
+}
+
