d10a033495352ba8d8ac71f81129f0652526ac5e
shriya-egov-tarento <shriya.bajpai@egovernments.org>
2019-12-06 14:36:48 +0530

    added validation for water services

--- ws-services/src/main/java/org/egov/waterConnection/validator/WaterConnectionValidator.java
+++ ws-services/src/main/java/org/egov/waterConnection/validator/WaterConnectionValidator.java
@@ -8,6 +8,7 @@ import org.egov.tracer.model.CustomException;
 import org.egov.waterConnection.model.WaterConnection;
 import org.egov.waterConnection.model.WaterConnectionRequest;
 import org.egov.waterConnection.repository.WaterDao;
+import org.egov.waterConnection.util.WCConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,8 +22,11 @@ public class WaterConnectionValidator {
 
 	/**
 	 * 
-	 * @param waterConnectionRequest WaterConnectionRequest is request for create or update water connection
-	 * @param isUpdate True for update and false for create
+	 * @param waterConnectionRequest
+	 *            WaterConnectionRequest is request for create or update water
+	 *            connection
+	 * @param isUpdate
+	 *            True for update and false for create
 	 */
 	public void validateWaterConnection(WaterConnectionRequest waterConnectionRequest, boolean isUpdate) {
 		WaterConnection waterConnection = waterConnectionRequest.getWaterConnection();
@@ -30,6 +34,17 @@ public class WaterConnectionValidator {
 		if (isUpdate && (waterConnection.getId() == null || waterConnection.getId().isEmpty())) {
 			errorMap.put("INVALID WATER CONNECTION", "WaterConnection cannot be update without connection id");
 		}
+		if (waterConnection.getConnectionType() == WCConstants.METER_STATUS) {
+			if (waterConnection.getMeterId() == null) {
+				errorMap.put("INVALID WATER CONNECTION", "Meter Id cannot be null !!");
+			}
+			if (waterConnection.getMeterInstallationDate() < 0 || waterConnection.getMeterInstallationDate() == null
+					|| waterConnection.getMeterInstallationDate() == 0) {
+				errorMap.put("INVALID WATER CONNECTION", "Meter Installation date cannot be null or negative !!");
+			}
+		}
+			
+	 
 		if (isUpdate && waterConnection.getId() != null && !waterConnection.getId().isEmpty()) {
 			int n = waterDao.isWaterConnectionExist(Arrays.asList(waterConnection.getId()));
 			if (n == 0) {
@@ -37,10 +52,10 @@ public class WaterConnectionValidator {
 			}
 		}
 		if (waterConnection.getConnectionType() == null || waterConnection.getConnectionType().isEmpty()) {
-			errorMap.put("INVALID WATER CONNECTION", "WaterConnection cannot be updated without connection type");
+			errorMap.put("INVALID WATER CONNECTION", "WaterConnection cannot be created  without connection type");
 		}
 		if (waterConnection.getConnectionCategory() == null || waterConnection.getConnectionCategory().isEmpty()) {
-			errorMap.put("INVALID WATER CONNECTION", "WaterConnection cannot be update without connection category");
+			errorMap.put("INVALID WATER CONNECTION", "WaterConnection cannot be created without connection category");
 		}
 		if (waterConnection.getWaterSource() == null || waterConnection.getWaterSource().isEmpty()) {
 			errorMap.put("INVALID WATER CONNECTION", "WaterConnection cannot be created without water source");
