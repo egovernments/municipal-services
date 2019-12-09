11cd5605c8c3602f230858d528494adece0baaa0
Amit-yadav-egov-tarento <amit.yadav@egovernments.org>
2019-12-09 17:15:00 +0530

    Demand Payer Object Added in User Model

--- ws-calculator/src/main/java/org/egov/wsCalculation/service/DemandService.java
+++ ws-calculator/src/main/java/org/egov/wsCalculation/service/DemandService.java
@@ -156,8 +156,8 @@ public class DemandService {
 
 			String tenantId = calculation.getTenantId();
 			String consumerCode = calculation.getConnectionNo();
-			User owner = requestInfo.getUserInfo();
-
+			User owner = connection.getProperty().getOwners().get(0).toCommonUser();
+			
 			List<DemandDetail> demandDetails = new LinkedList<>();
 
 			calculation.getTaxHeadEstimates().forEach(taxHeadEstimate -> {
