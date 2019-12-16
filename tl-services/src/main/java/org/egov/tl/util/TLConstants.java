b32759d06c99105e8b7d61f07dab5480243dd00b
talele08 <talele.aniket@gmail.com>
2019-12-16 15:42:19 +0530

    integration of workflow_2.0 with TL

--- tl-services/src/main/java/org/egov/tl/util/TLConstants.java
+++ tl-services/src/main/java/org/egov/tl/util/TLConstants.java
@@ -174,6 +174,8 @@ public class TLConstants {
 	public static final String  USREVENTS_EVENT_NAME = "Trade License";
 	public static final String  USREVENTS_EVENT_POSTEDBY = "SYSTEM-TL";
 
+	public static final String CITIZEN_SENDBACK_ACTION = "SENDBACKTOCITIZEN";
+
 
     public static final List<String> FIELDS_TO_IGNORE = Collections.unmodifiableList(Arrays.asList(VARIABLE_ACTION,VARIABLE_WFDOCUMENTS,
             VARIABLE_CREATEDBY,VARIABLE_LASTMODIFIEDBY,VARIABLE_CREATEDTIME,VARIABLE_LASTMODIFIEDTIME));
