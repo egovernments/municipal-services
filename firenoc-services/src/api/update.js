a1c6f27d5f1a875573ba6eb91fc69a0d4be3e3b4
rohit-egov <rohit.shende@egovernments.org>
2019-12-19 16:28:18 +0530

    minor changes in fire noc

--- firenoc-services/src/api/update.js
+++ firenoc-services/src/api/update.js
@@ -30,6 +30,7 @@ export default ({ config }) => {
   return api;
 };
 export const updateApiResponse = async ({ body }, next) => {
+  console.log("Update Body: "+body);
   let payloads = [];
   let mdms = await mdmsData(body.RequestInfo, body.FireNOCs[0].tenantId);
   //model validator
