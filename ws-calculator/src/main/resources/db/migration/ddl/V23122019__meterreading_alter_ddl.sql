c0a21dfa4248c3b6242ba6eea34462344697f37c
Amit-yadav-egov-tarento <amit.yadav@egovernments.org>
2019-12-23 14:56:01 +0530

    Meter Reading DDl Change

--- ws-calculator/src/main/resources/db/migration/ddl/V23122019__meterreading_alter_ddl.sql
+++ ws-calculator/src/main/resources/db/migration/ddl/V23122019__meterreading_alter_ddl.sql
@@ -1,4 +1,4 @@
 ALTER TABLE meterreading
 ADD COLUMN consumption decimal;
-ALTER TABLE meterreading ALTER COLUMN currentReading TYPE anytype;
-ALTER TABLE meterreading ALTER COLUMN lastReading TYPE anytype;
+ALTER TABLE meterreading ALTER COLUMN currentReading TYPE decimal;
+ALTER TABLE meterreading ALTER COLUMN lastReading TYPE decimal;
