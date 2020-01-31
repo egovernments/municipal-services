6e0cbedb3c193b33bfa771e794553d7668e876d1
shriya-egov-tarento <shriya.bajpai@egovernments.org>
2020-01-31 17:55:34 +0530

    Notification Flow Added

--- ws-services/src/main/java/org/egov/waterConnection/config/WSConfiguration.java
+++ ws-services/src/main/java/org/egov/waterConnection/config/WSConfiguration.java
@@ -23,48 +23,96 @@ public class WSConfiguration {
 
 	@Value("${egov.waterservice.pagination.default.offset}")
 	private Integer defaultOffset;
-	
+
 	@Value("${egov.waterservice.pagination.max.limit}")
 	private Integer maxLimit;
+
+	// IDGEN
+	@Value("${egov.idgen.wcid.name}")
+	private String waterConnectionIdGenName;
+
+	@Value("${egov.idgen.wcid.format}")
+	private String waterConnectionIdGenFormat;
+
+	@Value("${egov.idgen.wcapid.name}")
+	private String waterApplicationIdGenName;
+
+	@Value("${egov.idgen.wcapid.format}")
+	private String waterApplicationIdGenFormat;
+
+	// Idgen Config
+	@Value("${egov.idgen.host}")
+	private String idGenHost;
+
+	@Value("${egov.idgen.path}")
+	private String idGenPath;
+
+	// Workflow
+	@Value("${create.ws.workflow.name}")
+	private String businessServiceValue;
+
+	@Value("${workflow.context.path}")
+	private String wfHost;
+
+	@Value("${workflow.transition.path}")
+	private String wfTransitionPath;
+
+	@Value("${workflow.businessservice.search.path}")
+	private String wfBusinessServiceSearchPath;
+
+	@Value("${is.external.workflow.enabled}")
+	private Boolean isExternalWorkFlowEnabled;
+
+	@Value("${egov.waterservice.updatewaterconnection.workflow.topic}")
+	private String workFlowUpdateTopic;
+
+	// Localization
+	@Value("${egov.localization.host}")
+	private String localizationHost;
+
+	@Value("${egov.localization.context.path}")
+	private String localizationContextPath;
+
+	@Value("${egov.localization.search.endpoint}")
+	private String localizationSearchEndpoint;
+
+	@Value("${egov.localization.statelevel}")
+	private Boolean isLocalizationStateLevel;
+
+	// SMS
+	@Value("${kafka.topics.notification.sms}")
+	private String smsNotifTopic;
+
+	@Value("${notification.sms.enabled}")
+	private Boolean isSMSEnabled;
+
+	@Value("${notification.sms.link}")
+	private String smsNotificationLink;
+
+	@Value("${notification.url}")
+	private String notificationUrl;
+
+	@Value("${egov.usr.events.create.topic}")
+	private String saveUserEventsTopic;
+
+	// Water Topic
+	@Value("${egov.waterservice.createwaterconnection}")
+	private String onWaterSaved;
+
+	@Value("${egov.waterservice.updatewaterconnection}")
+	private String onWaterUpdated;
+
+	@Value("${egov.user.event.notification.enabled}")
+	private Boolean isUserEventsNotificationEnabled;
 	
-	 //IDGEN
-    @Value("${egov.idgen.wcid.name}")
-    private String waterConnectionIdGenName;
+    //User Configuration
+    @Value("${egov.user.host}")
+    private String userHost;
 
-    @Value("${egov.idgen.wcid.format}")
-    private String waterConnectionIdGenFormat;
-    
-    @Value("${egov.idgen.wcapid.name}")
-    private String waterApplicationIdGenName;
+    @Value("${egov.user.context.path}")
+    private String userContextPath;
 
-    @Value("${egov.idgen.wcapid.format}")
-    private String waterApplicationIdGenFormat;
-    
-    //Idgen Config
-    @Value("${egov.idgen.host}")
-    private String idGenHost;
-
-    @Value("${egov.idgen.path}")
-    private String idGenPath;
-    
-    
- // Workflow
-    @Value("${create.ws.workflow.name}")
-    private String businessServiceValue;
-
-    @Value("${workflow.context.path}")
-    private String wfHost;
-
-    @Value("${workflow.transition.path}")
-    private String wfTransitionPath;
-
-    @Value("${workflow.businessservice.search.path}")
-    private String wfBusinessServiceSearchPath;
-
-    @Value("${is.external.workflow.enabled}")
-    private Boolean isExternalWorkFlowEnabled;
-    
-    @Value("${egov.waterservice.updatewaterconnection.workflow.topic}")
-    private String workFlowUpdateTopic;
+    @Value("${egov.user.search.path}")
+    private String userSearchEndpoint;
 
 }
