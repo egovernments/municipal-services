7e66ffe36b43fe7319dd9a09d0327045048f262f
shriya-egov-tarento <shriya.bajpai@egovernments.org>
2020-02-14 16:46:42 +0530

    model changes in sewerage

--- sw-calculator/src/main/java/org/egov/swCalculation/model/SewerageConnection.java
+++ sw-calculator/src/main/java/org/egov/swCalculation/model/SewerageConnection.java
@@ -8,7 +8,6 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.math.BigDecimal;
 import java.util.List;
-
 import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
 import javax.validation.constraints.*;
@@ -19,202 +18,223 @@ import javax.validation.constraints.*;
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-12-02T14:54:15.890+05:30[Asia/Kolkata]")
 public class SewerageConnection extends Connection {
-	@JsonProperty("connectionExecutionDate")
-	private BigDecimal connectionExecutionDate = null;
+	 @JsonProperty("connectionExecutionDate")
+	  private BigDecimal connectionExecutionDate = null;
 
-	@JsonProperty("proposedWaterClosets")
-	private Integer proposedWaterClosets = null;
+	  @JsonProperty("proposedWaterClosets")
+	  private Integer proposedWaterClosets = null;
 
-	@JsonProperty("proposedToilets")
-	private Integer proposedToilets = null;
+	  @JsonProperty("proposedToilets")
+	  private Integer proposedToilets = null;
 
-	@JsonProperty("noOfWaterClosets")
-	private Integer noOfWaterClosets = null;
+	  @JsonProperty("noOfWaterClosets")
+	  private Integer noOfWaterClosets = null;
 
-	@JsonProperty("noOfToilets")
-	private Integer noOfToilets = null;
+	  @JsonProperty("noOfToilets")
+	  private Integer noOfToilets = null;
 
-	@JsonProperty("uom")
-	private String uom = null;
+	  @JsonProperty("uom")
+	  private String uom = null;
 
-	@JsonProperty("connectionType")
-	private String connectionType = null;
+	  @JsonProperty("calculationAttribute")
+	  private String calculationAttribute = null;
 
-	public SewerageConnection connectionExecutionDate(BigDecimal connectionExecutionDate) {
-		this.connectionExecutionDate = connectionExecutionDate;
-		return this;
-	}
+	  @JsonProperty("connectionType")
+	  private String connectionType = null;
 
-	/**
-	 * Get connectionExecutionDate
-	 * 
-	 * @return connectionExecutionDate
-	 **/
-	@ApiModelProperty(required = true, readOnly = true, value = "")
-	@Valid
-	public BigDecimal getConnectionExecutionDate() {
-		return connectionExecutionDate;
-	}
+	  public SewerageConnection connectionExecutionDate(BigDecimal connectionExecutionDate) {
+	    this.connectionExecutionDate = connectionExecutionDate;
+	    return this;
+	  }
 
-	public void setConnectionExecutionDate(BigDecimal connectionExecutionDate) {
-		this.connectionExecutionDate = connectionExecutionDate;
-	}
+	  /**
+	   * Get connectionExecutionDate
+	   * @return connectionExecutionDate
+	  **/
+	  @ApiModelProperty(required = true, readOnly = true, value = "")
+	  @Valid
+	  public BigDecimal getConnectionExecutionDate() {
+	    return connectionExecutionDate;
+	  }
 
-	public SewerageConnection noOfWaterClosets(Integer noOfWaterClosets) {
-		this.noOfWaterClosets = noOfWaterClosets;
-		return this;
-	}
+	  public void setConnectionExecutionDate(BigDecimal connectionExecutionDate) {
+	    this.connectionExecutionDate = connectionExecutionDate;
+	  }
 
-	/**
-	 * No of taps for water closets of calculation attribute.
-	 * 
-	 * @return noOfWaterClosets
-	 **/
-	@ApiModelProperty(value = "No of taps for water closets of calculation attribute.")
+	  public SewerageConnection noOfWaterClosets(Integer noOfWaterClosets) {
+	    this.noOfWaterClosets = noOfWaterClosets;
+	    return this;
+	  }
 
-	public Integer getNoOfWaterClosets() {
-		return noOfWaterClosets;
-	}
+	  /**
+	   * No of taps for water closets of calculation attribute.
+	   * @return noOfWaterClosets
+	  **/
+	  @ApiModelProperty(value = "No of taps for water closets of calculation attribute.")
 
-	public void setNoOfWaterClosets(Integer noOfWaterClosets) {
-		this.noOfWaterClosets = noOfWaterClosets;
-	}
+	  public Integer getNoOfWaterClosets() {
+	    return noOfWaterClosets;
+	  }
 
-	public SewerageConnection noOfToilets(Integer noOfToilets) {
-		this.noOfToilets = noOfToilets;
-		return this;
-	}
+	  public void setNoOfWaterClosets(Integer noOfWaterClosets) {
+	    this.noOfWaterClosets = noOfWaterClosets;
+	  }
 
-	/**
-	 * No of taps for toilets of calculation attribute.
-	 * 
-	 * @return noOfToilets
-	 **/
-	@ApiModelProperty(value = "No of taps for toilets of calculation attribute.")
+	  public SewerageConnection noOfToilets(Integer noOfToilets) {
+	    this.noOfToilets = noOfToilets;
+	    return this;
+	  }
 
-	public Integer getNoOfToilets() {
-		return noOfToilets;
-	}
+	  /**
+	   * No of taps for toilets of calculation attribute.
+	   * @return noOfToilets
+	  **/
+	  @ApiModelProperty(value = "No of taps for toilets of calculation attribute.")
 
-	public void setNoOfToilets(Integer noOfToilets) {
-		this.noOfToilets = noOfToilets;
-	}
+	  public Integer getNoOfToilets() {
+	    return noOfToilets;
+	  }
 
-	public SewerageConnection uom(String uom) {
-		this.uom = uom;
-		return this;
-	}
+	  public void setNoOfToilets(Integer noOfToilets) {
+	    this.noOfToilets = noOfToilets;
+	  }
 
-	@ApiModelProperty(value = "No of proposed water closets")
+	  public SewerageConnection uom(String uom) {
+	    this.uom = uom;
+	    return this;
+	  }
 
-	public Integer getProposedWaterClosets() {
-		return proposedWaterClosets;
-	}
 
-	public void setProposedWaterClosets(Integer proposedWaterClosets) {
-		this.proposedWaterClosets = proposedWaterClosets;
-	}
 
-	public SewerageConnection proposedWaterClosets(Integer proposedWaterClosets) {
-		this.proposedWaterClosets = proposedWaterClosets;
-		return this;
-	}
+	  @ApiModelProperty(value = "No of proposed water closets")
 
-	@ApiModelProperty(value = "No of proposed toilets")
+	  public Integer getProposedWaterClosets() {
+	    return proposedWaterClosets;
+	  }
 
-	public Integer getProposedToilets() {
-		return proposedToilets;
-	}
+	  public void setProposedWaterClosets(Integer proposedWaterClosets) {
+	    this.proposedWaterClosets = proposedWaterClosets;
+	  }
 
-	public void setProposedToilets(Integer proposedToilets) {
-		this.proposedToilets = proposedToilets;
-	}
+	  public SewerageConnection proposedWaterClosets(Integer proposedWaterClosets) {
+	    this.proposedWaterClosets = proposedWaterClosets;
+	    return this;
+	  }
 
-	public SewerageConnection proposedToilets(Integer proposedToilets) {
-		this.proposedToilets = proposedToilets;
-		return this;
-	}
 
-	/**
-	 * The Unit of measurement for sewarage connection.
-	 * 
-	 * @return uom
-	 **/
-	@ApiModelProperty(required = true, value = "The Unit of measurement for sewarage connection.")
-	@Size(min = 2, max = 32)
-	public String getUom() {
-		return uom;
-	}
+	  @ApiModelProperty(value = "No of proposed toilets")
 
-	public void setUom(String uom) {
-		this.uom = uom;
-	}
+	  public Integer getProposedToilets() {
+	    return proposedToilets;
+	  }
 
-	public SewerageConnection connectionType(String connectionType) {
-		this.connectionType = connectionType;
-		return this;
-	}
+	  public void setProposedToilets(Integer proposedToilets) {
+	    this.proposedToilets = proposedToilets;
+	  }
 
-	/**
-	 * It is a master data, defined in MDMS.
-	 * 
-	 * @return connectionType
-	 **/
-	@ApiModelProperty(required = true, value = "It is a master data, defined in MDMS.")
+	  public SewerageConnection proposedToilets(Integer proposedToilets) {
+	    this.proposedToilets = proposedToilets;
+	    return this;
+	  }
 
-	@Size(min = 2, max = 32)
-	public String getConnectionType() {
-		return connectionType;
-	}
+	  /**
+	   * The Unit of measurement for sewarage connection.
+	   * @return uom
+	  **/
+	  @ApiModelProperty(required = true, value = "The Unit of measurement for sewarage connection.")
+	@Size(min=2,max=32)   public String getUom() {
+	    return uom;
+	  }
 
-	public void setConnectionType(String connectionType) {
-		this.connectionType = connectionType;
-	}
+	  public void setUom(String uom) {
+	    this.uom = uom;
+	  }
 
-	@Override
-	public boolean equals(java.lang.Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		SewerageConnection sewerageConnection = (SewerageConnection) o;
-		return Objects.equals(this.connectionExecutionDate, sewerageConnection.connectionExecutionDate)
-				&& Objects.equals(this.noOfWaterClosets, sewerageConnection.noOfWaterClosets)
-				&& Objects.equals(this.noOfToilets, sewerageConnection.noOfToilets)
-				&& Objects.equals(this.uom, sewerageConnection.uom)
-				&& Objects.equals(this.connectionType, sewerageConnection.connectionType) && super.equals(o);
-	}
+	  public SewerageConnection calculationAttribute(String calculationAttribute) {
+	    this.calculationAttribute = calculationAttribute;
+	    return this;
+	  }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(connectionExecutionDate, noOfWaterClosets, noOfToilets, uom, connectionType,
-				super.hashCode());
-	}
+	  /**
+	   * The calculation attribute of sewarage connection.
+	   * @return calculationAttribute
+	  **/
+	  @ApiModelProperty(required = true, value = "The calculation attribute of sewarage connection.")
 
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("class SewerageConnection {\n");
-		sb.append("    ").append(toIndentedString(super.toString())).append("\n");
-		sb.append("    connectionExecutionDate: ").append(toIndentedString(connectionExecutionDate)).append("\n");
-		sb.append("    noOfWaterClosets: ").append(toIndentedString(noOfWaterClosets)).append("\n");
-		sb.append("    noOfToilets: ").append(toIndentedString(noOfToilets)).append("\n");
-		sb.append("    uom: ").append(toIndentedString(uom)).append("\n");
-		sb.append("    connectionType: ").append(toIndentedString(connectionType)).append("\n");
-		sb.append("}");
-		return sb.toString();
-	}
+	@Size(min=2,max=32)   public String getCalculationAttribute() {
+	    return calculationAttribute;
+	  }
 
-	/**
-	 * Convert the given object to string with each line indented by 4 spaces
-	 * (except the first line).
-	 */
-	private String toIndentedString(java.lang.Object o) {
-		if (o == null) {
-			return "null";
-		}
-		return o.toString().replace("\n", "\n    ");
-	}
+	  public void setCalculationAttribute(String calculationAttribute) {
+	    this.calculationAttribute = calculationAttribute;
+	  }
+
+	  public SewerageConnection connectionType(String connectionType) {
+	    this.connectionType = connectionType;
+	    return this;
+	  }
+
+	  /**
+	   * It is a master data, defined in MDMS.
+	   * @return connectionType
+	  **/
+	  @ApiModelProperty(required = true, value = "It is a master data, defined in MDMS.")
+
+	@Size(min=2,max=32)   public String getConnectionType() {
+	    return connectionType;
+	  }
+
+	  public void setConnectionType(String connectionType) {
+	    this.connectionType = connectionType;
+	  }
+
+
+	  @Override
+	  public boolean equals(java.lang.Object o) {
+	    if (this == o) {
+	      return true;
+	    }
+	    if (o == null || getClass() != o.getClass()) {
+	      return false;
+	    }
+	    SewerageConnection sewerageConnection = (SewerageConnection) o;
+	    return Objects.equals(this.connectionExecutionDate, sewerageConnection.connectionExecutionDate) &&
+	        Objects.equals(this.noOfWaterClosets, sewerageConnection.noOfWaterClosets) &&
+	        Objects.equals(this.noOfToilets, sewerageConnection.noOfToilets) &&
+	        Objects.equals(this.uom, sewerageConnection.uom) &&
+	        Objects.equals(this.calculationAttribute, sewerageConnection.calculationAttribute) &&
+	        Objects.equals(this.connectionType, sewerageConnection.connectionType) &&
+	        super.equals(o);
+	  }
+
+	  @Override
+	  public int hashCode() {
+	    return Objects.hash(connectionExecutionDate, noOfWaterClosets, noOfToilets, uom, calculationAttribute, connectionType, super.hashCode());
+	  }
+
+	  @Override
+	  public String toString() {
+	    StringBuilder sb = new StringBuilder();
+	    sb.append("class SewerageConnection {\n");
+	    sb.append("    ").append(toIndentedString(super.toString())).append("\n");
+	    sb.append("    connectionExecutionDate: ").append(toIndentedString(connectionExecutionDate)).append("\n");
+	    sb.append("    noOfWaterClosets: ").append(toIndentedString(noOfWaterClosets)).append("\n");
+	    sb.append("    noOfToilets: ").append(toIndentedString(noOfToilets)).append("\n");
+	    sb.append("    uom: ").append(toIndentedString(uom)).append("\n");
+	    sb.append("    calculationAttribute: ").append(toIndentedString(calculationAttribute)).append("\n");
+	    sb.append("    connectionType: ").append(toIndentedString(connectionType)).append("\n");
+	    sb.append("}");
+	    return sb.toString();
+	  }
+
+	  /**
+	   * Convert the given object to string with each line indented by 4 spaces
+	   * (except the first line).
+	   */
+	  private String toIndentedString(java.lang.Object o) {
+	    if (o == null) {
+	      return "null";
+	    }
+	    return o.toString().replace("\n", "\n    ");
+	  }
 }
