33c1a4cd6891e9d6d48a46c29e0e292b312b03cf
veswantht-wtt-egov <veswanthraju.thokachichu@egovernments.org>
2020-04-02 15:22:43 +0530

    Persister file issue fix for tradeType

--- bpa-services/src/main/java/org/egov/bpa/web/controller/BPAController.java
+++ bpa-services/src/main/java/org/egov/bpa/web/controller/BPAController.java
@@ -27,7 +27,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BPAController {
