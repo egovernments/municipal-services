a6beb0053592cdc71cc7b256aeb975aa042e4922
lakshmisravanikolla-wtt-egov <57343149+lakshmisravanikolla-wtt-egov@users.noreply.github.com>
2020-04-20 10:36:25 +0530

    migration file chnage in the db

--- bpa-services/src/main/resources/db/migration/main/V20200416150430__blocks_units.sql
+++ bpa-services/src/main/resources/db/migration/main/V20200416150430__blocks_units.sql
@@ -1,3 +1,4 @@
+ALTER TABLE eg_bpa_unit ALTER COLUMN usagecategory TYPE character varying(256);
 
 INSERT INTO eg_bpa_unit(id,buildingplanid,usagecategory,createdby,lastmodifiedby,createdtime,lastmodifiedtime) SELECT id,buildingplanid,suboccupancytype,createdby,lastmodifiedby,createdtime,lastmodifiedtime FROM eg_bpa_blocks;
 
