c94b17765c76bfa4e3661b9d0a0e89961c1da3bd
rani-priyanka <priyanka.rani@egovernments.org>
2020-04-21 17:06:37 +0530

    [IP-384]:Configuring UOM based trade rates- removing uom from search criteria.

--- tl-calculator/src/main/java/org/egov/tlcalculator/service/CalculationService.java
+++ tl-calculator/src/main/java/org/egov/tlcalculator/service/CalculationService.java
@@ -239,7 +239,7 @@ public class CalculationService {
               searchCriteria.setTradeType(tradeUnit.getTradeType());
               if(tradeUnit.getUomValue()!=null)
               {
-                  searchCriteria.setUomValue(Double.parseDouble(tradeUnit.getUomValue()));
+                  //searchCriteria.setUomValue(Double.parseDouble(tradeUnit.getUomValue()));
                   searchCriteria.setUom(tradeUnit.getUom());
               }
               // Call the Search
