69bc8a72c43af509e3d2f923720cbc4a1b51ef83
lakshhmisravani-wtt-egov <lakshmisravani.kolla@egovernments.org>
2020-04-22 21:19:59 +0530

    Code changes in migration file change.

--- bpa-services/src/main/resources/db/migration/main/V20200422171625__units_constraints.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE eg_bpa_unit
-ADD CONSTRAINT eg_bpa_unit_uniqye UNIQUE (id,tenantId,buildingplanid,blockindex);
--- bpa-services/src/main/resources/db/migration/main/V20200422211325__units_constraints.sql
+++ bpa-services/src/main/resources/db/migration/main/V20200422211325__units_constraints.sql
@@ -1,3 +1,5 @@
+ALTER TABLE eg_bpa_unit
+ADD CONSTRAINT eg_bpa_unit_uniqye UNIQUE (id,tenantId,buildingplanid,blockindex);
 
 UPDATE public.eg_bpa_unit
 SET tenantid='bh.sonpur' WHERE tenantid is null;
