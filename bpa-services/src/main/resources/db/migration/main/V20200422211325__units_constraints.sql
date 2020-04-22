9e48a313a8657dbe290df0c1fcbe21049c222884
lakshhmisravani-wtt-egov <lakshmisravani.kolla@egovernments.org>
2020-04-22 21:56:55 +0530

    simple change.

--- bpa-services/src/main/resources/db/migration/main/V20200422211325__units_constraints.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE eg_bpa_unit
-ADD CONSTRAINT eg_bpa_unit_uniqye UNIQUE (id,tenantId,buildingplanid,blockindex);
-
-UPDATE public.eg_bpa_unit
-SET tenantid='bh.sonpur' WHERE tenantid is null;
-UPDATE public.eg_bpa_unit
-SET blockIndex=0 WHERE blockIndex is null;
