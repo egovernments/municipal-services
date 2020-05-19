b127ed17e5d0311218fb5cddd7dad56642669c1e
veswantht-wtt-egov <veswanthraju.thokachichu@egovernments.org>
2020-05-19 13:36:54 +0530

    Migration file name changed

--- bpa-services/src/main/resources/db/migration/main/V20200513124650__rename_tables.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE IF EXISTS eg_bpa_buildingplan RENAME TO eg_bpa_buildingplan_temp;
-ALTER TABLE IF EXISTS eg_bpa_buildingplan_temp RENAME CONSTRAINT pk_eg_bpa_buildingplan TO pk_eg_bpa_buildingplan_temp;
-ALTER TABLE IF EXISTS eg_bpa_auditdetails RENAME TO eg_bpa_auditdetails_temp;
-ALTER TABLE IF EXISTS eg_bpa_document RENAME TO eg_bpa_document_temp;
-ALTER TABLE IF EXISTS eg_bpa_document_temp RENAME CONSTRAINT uk_eg_bpa_document TO uk_eg_bpa_document_temp;
-ALTER TABLE IF EXISTS eg_bpa_document_temp RENAME CONSTRAINT fk_eg_bpa_document TO fk_eg_bpa_document_temp;
