9ec629078ad89f36e833472ab9f2ca55a6aabf96
egov <karan.bajrang@egovernments.org>
2020-05-25 19:58:14 +0530

    UKIMPEL-179:Fix for two SMS issue when owner and payer same

--- tl-services/src/main/java/org/egov/tl/service/notification/PaymentNotificationService.java
+++ tl-services/src/main/java/org/egov/tl/service/notification/PaymentNotificationService.java
@@ -106,11 +106,15 @@ public class PaymentNotificationService {
 
             List<SMSRequest> totalSMS = new LinkedList<>();
             totalSMS.addAll(ownersSMSRequest);
-            totalSMS.add(payerSMSRequest);
+            String payerMobileNumber=valMap.get(payerMobileNumberKey);
+            long count= license.getTradeLicenseDetail().getOwners().stream().filter(owner->owner.getMobileNumber().equals(payerMobileNumber)).count();
+           		 		 
+            if(count==0){
+              totalSMS.add(payerSMSRequest);
+            }
 
             return totalSMS;
     }
-
 
     /**
      * Creates SMSRequest for the owners
