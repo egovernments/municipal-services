7d54814b9444458e3b6ff2e90c3c635b3c23d349
priyajais <priya.jaiswal@egovernments.org>
2020-06-22 21:09:42 +0530

    Added SMSRequest Model

---
+++ noc-services/src/main/java/org/egov/noc/util/NOCConstants.java
@@ -0,0 +1,9 @@
+package org.egov.noc.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NOCConstants {
+
+	public static final String SEARCH_MODULE = "rainmaker-bpa";
+}
