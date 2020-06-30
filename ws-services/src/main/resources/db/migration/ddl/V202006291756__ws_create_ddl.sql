492ae862cd4b6a0ea6a2856c96425e852d052434
Amit-yadav-egov-tarento <amit.yadav@egovernments.org>
2020-06-30 18:53:32 +0530

    WOR-336 modified column in connection holder table

--- ws-services/src/main/resources/db/migration/ddl/V202006291756__ws_create_ddl.sql
+++ ws-services/src/main/resources/db/migration/ddl/V202006291756__ws_create_ddl.sql
@@ -1,6 +1,6 @@
 CREATE TABLE eg_ws_connectionholder (
   tenantId            CHARACTER VARYING (256) NOT NULL,
-  application_id      CHARACTER VARYING (128) NOT NULL,
+  connectionid      CHARACTER VARYING (128) NOT NULL,
   status              CHARACTER VARYING (128) NOT NULL,
   userid              CHARACTER VARYING (128),
   isprimaryholder      BOOLEAN  NOT NULL,
@@ -12,6 +12,6 @@ CREATE TABLE eg_ws_connectionholder (
   createdtime         BIGINT NOT NULL,
   lastmodifiedby      CHARACTER VARYING (128),
   lastmodifiedtime    BIGINT,
-  CONSTRAINT pk_eg_ws_connectionholder PRIMARY KEY (userid, application_id),
-  CONSTRAINT fk_eg_ws_connectionholder FOREIGN KEY (applicationno) REFERENCES eg_ws_connection (id)
+  CONSTRAINT pk_eg_ws_connectionholder PRIMARY KEY (userid, connectionid),
+  CONSTRAINT fk_eg_ws_connectionholder FOREIGN KEY (connectionid) REFERENCES eg_ws_connection (id)
   );
