f3ac86c82905a14cd4431b50c27ac1c80d580686
karthik-egov-tarento <karthikeyan.rajendran@egovernments.com>
2020-07-02 10:26:36 +0530

    Fixed the topic configuration issue

--- ws-services/src/main/java/org/egov/waterconnection/consumer/ReceiptConsumer.java
+++ ws-services/src/main/java/org/egov/waterconnection/consumer/ReceiptConsumer.java
@@ -12,7 +12,7 @@ public class ReceiptConsumer {
 	@Autowired
 	private PaymentUpdateService paymentUpdateService;
 	
-	@KafkaListener(topics = {"${kafka.topics.receipt.create.topic}"})
+	@KafkaListener(topics = {"${kafka.topics.receipt.create}"})
     public void listenPayments(final HashMap<String, Object> record) {
         paymentUpdateService.process(record);
     }
