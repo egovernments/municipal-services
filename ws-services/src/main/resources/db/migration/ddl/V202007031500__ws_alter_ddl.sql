70fdb058b80ecb35a0d23b2f1a968a9167f05b30
karthik-egov-tarento <63232529+karthik-egov-tarento@users.noreply.github.com>
2020-07-03 15:42:49 +0530

    Removing temporary file

--- ws-services/src/main/resources/db/migration/ddl/V202007031500__ws_alter_ddl.sql
+++
@@ -1,7 +0,0 @@
-
-ALTER TABLE eg_ws_connection DROP COLUMN IF EXISTS applicationType, 
-DROP COLUMN IF EXISTS dateEffectiveFrom;
-
-
-ALTER TABLE eg_ws_connection_audit DROP COLUMN IF EXISTS applicationType,
-DROP COLUMN IF EXISTS dateEffectiveFrom;
