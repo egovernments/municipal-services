ba0f60d12fbca99db68e414afa03a2eb70993592
talele08 <talele08@users.noreply.github.com>
2020-07-10 14:44:53 +0530

    Update V20200103120231__update_billingslab.sql
    updated query

--- tl-calculator/src/main/resources/db/migration/main/V20200103120231__update_billingslab.sql
+++ tl-calculator/src/main/resources/db/migration/main/V20200103120231__update_billingslab.sql
@@ -1,3 +1,1 @@
-alter table eg_tl_billingslab add applicationtype character varying;
-
-update eg_tl_billingslab set applicationtype='NEW' where applicationtype='';
+alter table eg_tl_billingslab add applicationtype character varying DEFAULT 'NEW';
