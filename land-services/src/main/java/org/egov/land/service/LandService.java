5c7915c6cd6c78cfa20483dfbaac03a1c12ef7ec
veswantht-wtt-egov <veswanthraju.thokachichu@egovernments.org>
2020-07-14 13:31:47 +0530

    Fixed the landSearch with multi owners issue

--- land-services/src/main/java/org/egov/land/service/LandService.java
+++ land-services/src/main/java/org/egov/land/service/LandService.java
@@ -8,7 +8,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.egov.common.contract.request.RequestInfo;
-import org.egov.common.contract.request.Role;
 import org.egov.land.repository.LandRepository;
 import org.egov.land.util.LandConstants;
 import org.egov.land.util.LandUtil;
@@ -84,17 +83,18 @@ public class LandService {
 		landValidator.validateSearch(requestInfo, criteria);
 		if (criteria.getMobileNumber() != null) {
 			landInfo = getLandFromMobileNumber(criteria, requestInfo);
-		} else {
-			List<String> roles = new ArrayList<>();
-			for (Role role : requestInfo.getUserInfo().getRoles()) {
-				roles.add(role.getCode());
+			List<String> landIds = new ArrayList<String>();
+			for (LandInfo li : landInfo) {
+				landIds.add(li.getId());
 			}
-			
-			landInfo = getLandWithOwnerInfo(criteria, requestInfo);
+			criteria.setMobileNumber(null);
+			criteria.setIds(landIds);
 		}
 
-		if(!CollectionUtils.isEmpty(landInfo)){
-			log.debug("Received final landInfo response in service call..");			
+		landInfo = fetchLandInfoData(criteria, requestInfo);
+
+		if (!CollectionUtils.isEmpty(landInfo)) {
+			log.debug("Received final landInfo response in service call..");
 		}
 		return landInfo;
 	}
@@ -134,7 +134,7 @@ public class LandService {
 	 *            The search request's requestInfo
 	 * @return List of landInfo for the given criteria
 	 */
-	public List<LandInfo> getLandWithOwnerInfo(LandSearchCriteria criteria, RequestInfo requestInfo) {
+	public List<LandInfo> fetchLandInfoData(LandSearchCriteria criteria, RequestInfo requestInfo) {
 		List<LandInfo> landInfos = repository.getLandInfoData(criteria);
 		if (landInfos.isEmpty())
 			return Collections.emptyList();
