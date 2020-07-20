694ad408464ec67789720c032ceb46f105ce1700
talele08 <talele.aniket@gmail.com>
2020-07-20 15:13:03 +0530

    correction to mock data file path

--- pgr-services/src/main/java/org/egov/pgr/web/controllers/RequestsApiController.java
+++ pgr-services/src/main/java/org/egov/pgr/web/controllers/RequestsApiController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
 import org.egov.pgr.web.models.RequestInfoWrapper;
 import org.egov.pgr.web.models.RequestSearchCriteria;
+import org.egov.tracer.model.CustomException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
@@ -33,6 +37,7 @@ public class RequestsApiController{
 
     private final HttpServletRequest request;
 
+
     @Autowired
     public RequestsApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
@@ -41,28 +46,46 @@ public class RequestsApiController{
 
     @RequestMapping(value="/requests/_create", method = RequestMethod.POST)
     public ResponseEntity<String> requestsCreatePost() throws IOException {
-       String path = "/home/aniket/Documents/municipal-services/pgr-services/src/main/resources/mockData.json";
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
-        String res = new String(encoded,  StandardCharsets.US_ASCII);
-        return new ResponseEntity<>(res, HttpStatus.OK);
+        try{
+            URL url = getClass().getClassLoader().getResource("mockData.json");
+            byte[] encoded = Files.readAllBytes(Paths.get(url.toURI()));
+            String res = new String(encoded,  StandardCharsets.US_ASCII);
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            throw new CustomException("FILEPATH_ERROR","Failed to read file for mock data");
+        }
 
     }
 
     @RequestMapping(value="/requests/_search", method = RequestMethod.POST)
     public ResponseEntity<String> requestsSearchPost(@Valid @RequestBody RequestInfoWrapper requestInfoWrapper,
-                                                     @Valid @ModelAttribute RequestSearchCriteria criteria) throws IOException{
-        String path = "/home/aniket/Documents/municipal-services/pgr-services/src/main/resources/mockData.json";
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
-        String res = new String(encoded,  StandardCharsets.US_ASCII);
-        return new ResponseEntity<>(res, HttpStatus.OK);
+                                                     @Valid @ModelAttribute RequestSearchCriteria criteria) {
+     try{
+         URL url = getClass().getClassLoader().getResource("mockData.json");
+         byte[] encoded = Files.readAllBytes(Paths.get(url.toURI()));
+         String res = new String(encoded,  StandardCharsets.US_ASCII);
+         return new ResponseEntity<>(res, HttpStatus.OK);
+     }
+     catch (Exception e){
+         e.printStackTrace();
+         throw new CustomException("FILEPATH_ERROR","Failed to read file for mock data");
+     }
     }
 
     @RequestMapping(value="/requests/_update", method = RequestMethod.POST)
     public ResponseEntity<String> requestsUpdatePost() throws IOException {
-        String path = "/home/aniket/Documents/municipal-services/pgr-services/src/main/resources/mockData.json";
-        byte[] encoded = Files.readAllBytes(Paths.get(path));
-        String res = new String(encoded,  StandardCharsets.US_ASCII);
-        return new ResponseEntity<>(res, HttpStatus.OK);
+        try{
+            URL url = getClass().getClassLoader().getResource("mockData.json");
+            byte[] encoded = Files.readAllBytes(Paths.get(url.toURI()));
+            String res = new String(encoded,  StandardCharsets.US_ASCII);
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            throw new CustomException("FILEPATH_ERROR","Failed to read file for mock data");
+        }
     }
 
 }
