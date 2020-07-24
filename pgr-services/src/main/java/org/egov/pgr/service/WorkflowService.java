cf5711f4f40a2c8547e7cffd2aed1b1953c43d7d
talele08 <talele.aniket@gmail.com>
2020-07-24 10:51:30 +0530

    added stubs for workflow integration

---
+++ pgr-services/src/main/java/org/egov/pgr/service/WorkflowService.java
@@ -0,0 +1,46 @@
+package org.egov.pgr.service;
+
+import org.egov.pgr.web.models.ServiceRequest;
+import org.egov.pgr.web.models.workflow.BusinessService;
+
+@org.springframework.stereotype.Service
+public class WorkflowService {
+
+
+
+
+    public BusinessService getBusinessService(ServiceRequest serviceRequest){
+        /*
+        *
+        * Should return the applicable BusinessService for the given request
+        *
+        * */
+        return null;
+    }
+
+
+
+    public String updateWorkflowStatus(ServiceRequest serviceRequest){
+
+        /*
+        * Call the workflow service with the given action and update the status
+        * return the updated status of the application
+        *
+        * */
+        return null;
+    }
+
+    
+    public void validateAssignee(ServiceRequest serviceRequest){
+        /*
+        * Call HRMS service and validate of the assignee belongs to same department
+        * as the employee assigning it
+        *
+        * */
+
+    }
+
+
+
+
+}
