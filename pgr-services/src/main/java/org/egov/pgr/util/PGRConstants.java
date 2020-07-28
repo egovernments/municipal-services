cd92240209602dfce7303444016eb9a5eacf3b58
rohit-eGov <52992779+rohit-eGov@users.noreply.github.com>
2020-07-28 10:33:25 +0530

    RAIN-1303 Workflow integration in PGR (#370)

--- pgr-services/src/main/java/org/egov/pgr/util/PGRConstants.java
+++ pgr-services/src/main/java/org/egov/pgr/util/PGRConstants.java
@@ -8,11 +8,17 @@ import org.springframework.stereotype.Component;
 public class PGRConstants {
 
 
-    public static final String businessService_PGR = "PGR";
+    public static final String PGR_BUSINESSSERVICE = "PGR";
 
     public static final String USERTYPE_EMPLOYEE = "EMPLOYEE";
 
     public static final String USERTYPE_CITIZEN = "CITIZEN";
+
+    public static final String WORKFLOW_START_ACTION = "INITIATE";
+
+    public static final String PGR_WORKFLOW_CODE = "PGR";
+
+    public static final String PGR_MODULENAME = "PGR";
 
 
 
