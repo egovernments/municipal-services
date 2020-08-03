c524845bd643ba7aed4ae00f905c60d78314526b
Deependra-egov <47975393+Deependra-egov@users.noreply.github.com>
2020-08-03 17:27:10 +0530

    payment issue

--- tl-services/src/main/java/org/egov/tl/service/notification/TLNotificationService.java
+++ tl-services/src/main/java/org/egov/tl/service/notification/TLNotificationService.java
@@ -23,11 +23,11 @@ import static org.egov.tl.util.TLConstants.*;
 public class TLNotificationService {
 
 
-    private TLConfiguration config;
+	private TLConfiguration config;
 
-    private ServiceRequestRepository serviceRequestRepository;
+	private ServiceRequestRepository serviceRequestRepository;
 
-    private NotificationUtil util;
+	private NotificationUtil util;
 
 	private BPANotificationUtil bpaNotificationUtil;
 
@@ -42,13 +42,13 @@ public class TLNotificationService {
 		this.tlRenewalNotificationUtil = tlRenewalNotificationUtil;
 	}
 
-    /**
-     * Creates and send the sms based on the tradeLicenseRequest
-     * @param request The tradeLicenseRequest listenend on the kafka topic
-     */
-    public void process(TradeLicenseRequest request){
+	/**
+	 * Creates and send the sms based on the tradeLicenseRequest
+	 * @param request The tradeLicenseRequest listenend on the kafka topic
+	 */
+	public void process(TradeLicenseRequest request){
 
-        String businessService = request.getLicenses().isEmpty()?null:request.getLicenses().get(0).getBusinessService();
+		String businessService = request.getLicenses().isEmpty()?null:request.getLicenses().get(0).getBusinessService();
 		if (businessService == null)
 			businessService = businessService_TL;
 		switch(businessService)
@@ -89,16 +89,16 @@ public class TLNotificationService {
 				}
 				break;
 		}
-    }
+	}
 
-    /**
-     * Enriches the smsRequest with the customized messages
-     * @param request The tradeLicenseRequest from kafka topic
-     * @param smsRequests List of SMSRequets
-     */
-    private void enrichSMSRequest(TradeLicenseRequest request,List<SMSRequest> smsRequests){
-        String tenantId = request.getLicenses().get(0).getTenantId();
-        for(TradeLicense license : request.getLicenses()){
+	/**
+	 * Enriches the smsRequest with the customized messages
+	 * @param request The tradeLicenseRequest from kafka topic
+	 * @param smsRequests List of SMSRequets
+	 */
+	private void enrichSMSRequest(TradeLicenseRequest request,List<SMSRequest> smsRequests){
+		String tenantId = request.getLicenses().get(0).getTenantId();
+		for(TradeLicense license : request.getLicenses()){
 			String businessService = license.getBusinessService();
 			if (businessService == null)
 				businessService = businessService_TL;
@@ -123,31 +123,31 @@ public class TLNotificationService {
 				String localizationMessages = tlRenewalNotificationUtil.getLocalizationMessages(tenantId, request.getRequestInfo());
 				message = tlRenewalNotificationUtil.getCustomizedMsg(request.getRequestInfo(), license, localizationMessages);
 			}
-            if(message==null) continue;
+			if(message==null) continue;
 
-            Map<String,String > mobileNumberToOwner = new HashMap<>();
+			Map<String,String > mobileNumberToOwner = new HashMap<>();
 
-            license.getTradeLicenseDetail().getOwners().forEach(owner -> {
-                if(owner.getMobileNumber()!=null)
-                    mobileNumberToOwner.put(owner.getMobileNumber(),owner.getName());
-            });
-            smsRequests.addAll(util.createSMSRequest(message,mobileNumberToOwner));
-        }
-    }
-    
-    /**
-     * Creates and registers an event at the egov-user-event service at defined trigger points as that of sms notifs.
-     * 
-     * Assumption - The TradeLicenseRequest received will always contain only one TradeLicense.
-     * 
-     * @param request
-     * @return
-     */
-    private EventRequest getEventsForTL(TradeLicenseRequest request) {
-    	List<Event> events = new ArrayList<>();
-        String tenantId = request.getLicenses().get(0).getTenantId();
+			license.getTradeLicenseDetail().getOwners().forEach(owner -> {
+				if(owner.getMobileNumber()!=null)
+					mobileNumberToOwner.put(owner.getMobileNumber(),owner.getName());
+			});
+			smsRequests.addAll(util.createSMSRequest(message,mobileNumberToOwner));
+		}
+	}
+
+	/**
+	 * Creates and registers an event at the egov-user-event service at defined trigger points as that of sms notifs.
+	 *
+	 * Assumption - The TradeLicenseRequest received will always contain only one TradeLicense.
+	 *
+	 * @param request
+	 * @return
+	 */
+	private EventRequest getEventsForTL(TradeLicenseRequest request) {
+		List<Event> events = new ArrayList<>();
+		String tenantId = request.getLicenses().get(0).getTenantId();
 		String localizationMessages = util.getLocalizationMessages(tenantId,request.getRequestInfo());
-        for(TradeLicense license : request.getLicenses()){
+		for(TradeLicense license : request.getLicenses()){
 			String message = null;
 			String applicationType = String.valueOf(license.getApplicationType());
 			String businessService = license.getBusinessService();
@@ -157,58 +157,58 @@ public class TLNotificationService {
 				else
 					message = util.getCustomizedMsg(request.getRequestInfo(), license, localizationMessages);
 			}
-			
-            if(message == null) continue;
-            Map<String,String > mobileNumberToOwner = new HashMap<>();
-            license.getTradeLicenseDetail().getOwners().forEach(owner -> {
-                if(owner.getMobileNumber()!=null)
-                    mobileNumberToOwner.put(owner.getMobileNumber(),owner.getName());
-            });
-            List<SMSRequest> smsRequests = util.createSMSRequest(message,mobileNumberToOwner);
-        	Set<String> mobileNumbers = smsRequests.stream().map(SMSRequest :: getMobileNumber).collect(Collectors.toSet());
-        	Map<String, String> mapOfPhnoAndUUIDs = fetchUserUUIDs(mobileNumbers, request.getRequestInfo(), request.getLicenses().get(0).getTenantId());
-    		if (CollectionUtils.isEmpty(mapOfPhnoAndUUIDs.keySet())) {
-    			log.info("UUID search failed!");
-    			continue;
-    		}
-            Map<String,String > mobileNumberToMsg = smsRequests.stream().collect(Collectors.toMap(SMSRequest::getMobileNumber, SMSRequest::getMessage));		
-            for(String mobile: mobileNumbers) {
-    			if(null == mapOfPhnoAndUUIDs.get(mobile) || null == mobileNumberToMsg.get(mobile)) {
-    				log.error("No UUID/SMS for mobile {} skipping event", mobile);
-    				continue;
-    			}
-    			List<String> toUsers = new ArrayList<>();
-    			toUsers.add(mapOfPhnoAndUUIDs.get(mobile));
-    			Recepient recepient = Recepient.builder().toUsers(toUsers).toRoles(null).build();
-    			List<String> payTriggerList = Arrays.asList(config.getPayTriggers().split("[,]"));
-    			Action action = null;
-    			if(payTriggerList.contains(license.getStatus())) {
-                    List<ActionItem> items = new ArrayList<>();
-        			String actionLink = config.getPayLink().replace("$mobile", mobile)
-        						.replace("$applicationNo", license.getApplicationNumber())
-        						.replace("$tenantId", license.getTenantId())
-        						.replace("$businessService", license.getBusinessService());
-        			actionLink = config.getUiAppHost() + actionLink;
-        			ActionItem item = ActionItem.builder().actionUrl(actionLink).code(config.getPayCode()).build();
-        			items.add(item);
-        			action = Action.builder().actionUrls(items).build();
-    			}
 
-				
+			if(message == null) continue;
+			Map<String,String > mobileNumberToOwner = new HashMap<>();
+			license.getTradeLicenseDetail().getOwners().forEach(owner -> {
+				if(owner.getMobileNumber()!=null)
+					mobileNumberToOwner.put(owner.getMobileNumber(),owner.getName());
+			});
+			List<SMSRequest> smsRequests = util.createSMSRequest(message,mobileNumberToOwner);
+			Set<String> mobileNumbers = smsRequests.stream().map(SMSRequest :: getMobileNumber).collect(Collectors.toSet());
+			Map<String, String> mapOfPhnoAndUUIDs = fetchUserUUIDs(mobileNumbers, request.getRequestInfo(), request.getLicenses().get(0).getTenantId());
+			if (CollectionUtils.isEmpty(mapOfPhnoAndUUIDs.keySet())) {
+				log.info("UUID search failed!");
+				continue;
+			}
+			Map<String,String > mobileNumberToMsg = smsRequests.stream().collect(Collectors.toMap(SMSRequest::getMobileNumber, SMSRequest::getMessage));
+			for(String mobile: mobileNumbers) {
+				if(null == mapOfPhnoAndUUIDs.get(mobile) || null == mobileNumberToMsg.get(mobile)) {
+					log.error("No UUID/SMS for mobile {} skipping event", mobile);
+					continue;
+				}
+				List<String> toUsers = new ArrayList<>();
+				toUsers.add(mapOfPhnoAndUUIDs.get(mobile));
+				Recepient recepient = Recepient.builder().toUsers(toUsers).toRoles(null).build();
+				List<String> payTriggerList = Arrays.asList(config.getPayTriggers().split("[,]"));
+				Action action = null;
+				if(payTriggerList.contains(license.getStatus())) {
+					List<ActionItem> items = new ArrayList<>();
+					String actionLink = config.getPayLink().replace("$mobile", mobile)
+							.replace("$applicationNo", license.getApplicationNumber())
+							.replace("$tenantId", license.getTenantId())
+							.replace("$businessService", license.getBusinessService());
+					actionLink = config.getUiAppHost() + actionLink;
+					ActionItem item = ActionItem.builder().actionUrl(actionLink).code(config.getPayCode()).build();
+					items.add(item);
+					action = Action.builder().actionUrls(items).build();
+				}
+
+
 				events.add(Event.builder().tenantId(license.getTenantId()).description(mobileNumberToMsg.get(mobile))
 						.eventType(TLConstants.USREVENTS_EVENT_TYPE).name(TLConstants.USREVENTS_EVENT_NAME)
 						.postedBy(TLConstants.USREVENTS_EVENT_POSTEDBY).source(Source.WEBAPP).recepient(recepient)
 						.eventDetails(null).actions(action).build());
-    			
-    		}
-        }
-        if(!CollectionUtils.isEmpty(events)) {
-    		return EventRequest.builder().requestInfo(request.getRequestInfo()).events(events).build();
-        }else {
-        	return null;
-        }
-		
-    }
+
+			}
+		}
+		if(!CollectionUtils.isEmpty(events)) {
+			return EventRequest.builder().requestInfo(request.getRequestInfo()).events(events).build();
+		}else {
+			return null;
+		}
+
+	}
 
 	public EventRequest getEventsForBPA(TradeLicenseRequest request, boolean isStatusPaid, String paidMessage) {
 		List<Event> events = new ArrayList<>();
@@ -252,7 +252,7 @@ public class TLNotificationService {
 					String actionLink = config.getPayLink().replace("$mobile", mobile)
 							.replace("$applicationNo", license.getApplicationNumber())
 							.replace("$tenantId", license.getTenantId())
-					        .replace("$businessService", license.getBusinessService());;
+							.replace("$businessService", license.getBusinessService());;
 					actionLink = config.getUiAppHost() + actionLink;
 					ActionItem item = ActionItem.builder().actionUrl(actionLink).code(config.getPayCode()).build();
 					items.add(item);
@@ -274,43 +274,43 @@ public class TLNotificationService {
 		}
 
 	}
-    
-    
-    
-    /**
-     * Fetches UUIDs of CITIZENs based on the phone number.
-     * 
-     * @param mobileNumbers
-     * @param requestInfo
-     * @param tenantId
-     * @return
-     */
-    private Map<String, String> fetchUserUUIDs(Set<String> mobileNumbers, RequestInfo requestInfo, String tenantId) {
-    	Map<String, String> mapOfPhnoAndUUIDs = new HashMap<>();
-    	StringBuilder uri = new StringBuilder();
-    	uri.append(config.getUserHost()).append(config.getUserSearchEndpoint());
-    	Map<String, Object> userSearchRequest = new HashMap<>();
-    	userSearchRequest.put("RequestInfo", requestInfo);
+
+
+
+	/**
+	 * Fetches UUIDs of CITIZENs based on the phone number.
+	 *
+	 * @param mobileNumbers
+	 * @param requestInfo
+	 * @param tenantId
+	 * @return
+	 */
+	private Map<String, String> fetchUserUUIDs(Set<String> mobileNumbers, RequestInfo requestInfo, String tenantId) {
+		Map<String, String> mapOfPhnoAndUUIDs = new HashMap<>();
+		StringBuilder uri = new StringBuilder();
+		uri.append(config.getUserHost()).append(config.getUserSearchEndpoint());
+		Map<String, Object> userSearchRequest = new HashMap<>();
+		userSearchRequest.put("RequestInfo", requestInfo);
 		userSearchRequest.put("tenantId", tenantId);
 		userSearchRequest.put("userType", "CITIZEN");
-    	for(String mobileNo: mobileNumbers) {
-    		userSearchRequest.put("userName", mobileNo);
-    		try {
-    			Object user = serviceRequestRepository.fetchResult(uri, userSearchRequest);
-    			if(null != user) {
-    				String uuid = JsonPath.read(user, "$.user[0].uuid");
-    				mapOfPhnoAndUUIDs.put(mobileNo, uuid);
-    			}else {
-        			log.error("Service returned null while fetching user for username - "+mobileNo);
-    			}
-    		}catch(Exception e) {
-    			log.error("Exception while fetching user for username - "+mobileNo);
-    			log.error("Exception trace: ",e);
-    			continue;
-    		}
-    	}
-    	return mapOfPhnoAndUUIDs;
-    }
+		for(String mobileNo: mobileNumbers) {
+			userSearchRequest.put("userName", mobileNo);
+			try {
+				Object user = serviceRequestRepository.fetchResult(uri, userSearchRequest);
+				if(null != user) {
+					String uuid = JsonPath.read(user, "$.user[0].uuid");
+					mapOfPhnoAndUUIDs.put(mobileNo, uuid);
+				}else {
+					log.error("Service returned null while fetching user for username - "+mobileNo);
+				}
+			}catch(Exception e) {
+				log.error("Exception while fetching user for username - "+mobileNo);
+				log.error("Exception trace: ",e);
+				continue;
+			}
+		}
+		return mapOfPhnoAndUUIDs;
+	}
 
 
 
