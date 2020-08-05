09ead284e660d0203fea17a67ac681c3fe6fd2a0
talele08 <talele.aniket@gmail.com>
2020-08-05 12:14:57 +0530

    added workflow enrichment

--- pgr-services/src/main/java/org/egov/pgr/service/WorkflowService.java
+++ pgr-services/src/main/java/org/egov/pgr/service/WorkflowService.java
@@ -1,43 +1,44 @@
 package org.egov.pgr.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.egov.common.contract.request.User;
 import org.egov.pgr.config.PGRConfiguration;
 import org.egov.pgr.repository.ServiceRequestRepository;
-import org.egov.pgr.web.models.RequestInfoWrapper;
-import org.egov.pgr.web.models.Service;
-import org.egov.pgr.web.models.ServiceRequest;
-import org.egov.pgr.web.models.Status;
+import org.egov.pgr.web.models.*;
 import org.egov.pgr.web.models.workflow.*;
 import org.egov.tracer.model.CustomException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.egov.pgr.util.PGRConstants.*;
 
 @org.springframework.stereotype.Service
 public class WorkflowService {
 
-    @Autowired
     private PGRConfiguration pgrConfiguration;
 
-    @Autowired
     private ServiceRequestRepository repository;
 
-    @Autowired
     private ObjectMapper mapper;
 
 
+    @Autowired
+    public WorkflowService(PGRConfiguration pgrConfiguration, ServiceRequestRepository repository, ObjectMapper mapper) {
+        this.pgrConfiguration = pgrConfiguration;
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 
     /*
      *
      * Should return the applicable BusinessService for the given request
      *
      * */
-    public BusinessService getBusinessService(ServiceRequest serviceRequest){
+    public BusinessService getBusinessService(ServiceRequest serviceRequest) {
         String tenantId = serviceRequest.getPgrEntity().getService().getTenantId();
         StringBuilder url = getSearchURLWithParams(tenantId, PGR_BUSINESSSERVICE);
         RequestInfoWrapper requestInfoWrapper = RequestInfoWrapper.builder().requestInfo(serviceRequest.getRequestInfo()).build();
@@ -49,8 +50,8 @@ public class WorkflowService {
             throw new CustomException("PARSING ERROR", "Failed to parse response of workflow business service search");
         }
 
-        if(CollectionUtils.isEmpty(response.getBusinessServices()))
-            throw new CustomException("BUSINESSSERVICE_NOT_FOUND","The businessService "+PGR_BUSINESSSERVICE+" is not found");
+        if (CollectionUtils.isEmpty(response.getBusinessServices()))
+            throw new CustomException("BUSINESSSERVICE_NOT_FOUND", "The businessService " + PGR_BUSINESSSERVICE + " is not found");
 
         return response.getBusinessServices().get(0);
     }
@@ -61,27 +62,28 @@ public class WorkflowService {
      * return the updated status of the application
      *
      * */
-    public String updateWorkflowStatus(ServiceRequest serviceRequest){
+    public String updateWorkflowStatus(ServiceRequest serviceRequest) {
         ProcessInstance processInstance = getProcessInstanceForPGR(serviceRequest);
         ProcessInstanceRequest workflowRequest = new ProcessInstanceRequest(serviceRequest.getRequestInfo(), Collections.singletonList(processInstance));
         State state = callWorkFlow(workflowRequest);
+        serviceRequest.getPgrEntity().getService().setApplicationStatus(state.getApplicationStatus());
         return state.getApplicationStatus();
     }
 
 
-    public void validateAssignee(ServiceRequest serviceRequest){
+    public void validateAssignee(ServiceRequest serviceRequest) {
         /*
-        * Call HRMS service and validate of the assignee belongs to same department
-        * as the employee assigning it
-        *
-        * */
+         * Call HRMS service and validate of the assignee belongs to same department
+         * as the employee assigning it
+         *
+         * */
 
     }
 
     /**
      * Creates url for search based on given tenantId and businessservices
      *
-     * @param tenantId The tenantId for which url is generated
+     * @param tenantId        The tenantId for which url is generated
      * @param businessService The businessService for which url is generated
      * @return The search url
      */
@@ -97,25 +99,45 @@ public class WorkflowService {
     }
 
 
-    public void enrichmentForSendBackToCititzen(){
+    public void enrichmentForSendBackToCititzen() {
         /*
-        * If send bac to citizen action is taken assignes should be set to accountId
-        *
-        * */
+         * If send bac to citizen action is taken assignes should be set to accountId
+         *
+         * */
     }
 
 
-    public void enrichWorkflow(ServiceRequest request){
-        /*
-        * Should enrich the workflow object in PGREntity Object
-        * */
+    public void enrichWorkflow(ServiceRequest request) {
+
+        String tenantId = request.getPgrEntity().getService().getTenantId();
+        String serviceRequestId = request.getPgrEntity().getService().getServiceRequestId();
+        RequestInfoWrapper requestInfoWrapper = RequestInfoWrapper.builder().requestInfo(request.getRequestInfo()).build();
+
+        StringBuilder searchUrl = getprocessInstanceSearchURL(tenantId, serviceRequestId);
+        Object result = repository.fetchResult(searchUrl, requestInfoWrapper);
+
+
+        List<ProcessInstance> processInstances = new ArrayList<>();
+        try {
+            processInstances = mapper.convertValue(result, new TypeReference<List<ProcessInstance>>(){});
+        } catch (IllegalArgumentException e) {
+            throw new CustomException("PARSING ERROR", "Failed to parse response of workflow processInstance search");
+        }
+
+        if (CollectionUtils.isEmpty(processInstances))
+            throw new CustomException("WORKFLOW_NOT_FOUND", "The workflow for serviceRequestId:  " + serviceRequestId + " is not found");
+
+        Workflow workflow = getWorkflow(processInstances.get(0));
+        request.getPgrEntity().setWorkflow(workflow);
+
     }
 
-   /**
+    /**
      * Enriches ProcessInstance Object for workflow
+     *
      * @param serviceRequest
      */
-    public ProcessInstance getProcessInstanceForPGR(ServiceRequest serviceRequest){
+    public ProcessInstance getProcessInstanceForPGR(ServiceRequest serviceRequest) {
 
         Service service = serviceRequest.getPgrEntity().getService();
 
@@ -125,15 +147,38 @@ public class WorkflowService {
         processInstance.setModuleName(PGR_MODULENAME);
         processInstance.setTenantId(service.getTenantId());
         processInstance.setBusinessService(getBusinessService(serviceRequest).getBusinessService());
+        processInstance.setDocuments(serviceRequest.getPgrEntity().getWorkflow().getVerificationDocuments());
 
         return processInstance;
     }
-    
+
+    /**
+     *
+     * @param processInstance
+     */
+    public Workflow getWorkflow(ProcessInstance processInstance) {
+
+        List<String> userIds = null;
+
+        if(!CollectionUtils.isEmpty(processInstance.getAssignes())){
+            userIds = processInstance.getAssignes().stream().map(User::getUuid).collect(Collectors.toList());
+        }
+
+        Workflow workflow = Workflow.builder()
+                .action(processInstance.getAction())
+                .assignes(userIds)
+                .comments(processInstance.getComment())
+                .verificationDocuments(processInstance.getDocuments())
+                .build();
+
+        return workflow;
+    }
+
     /**
      * Method to integrate with workflow
-     *
+     * <p>
      * take the ProcessInstanceRequest as paramerter to call wf-service
-     *
+     * <p>
      * and return wf-response to sets the resultant status
      */
     public State callWorkFlow(ProcessInstanceRequest workflowReq) {
@@ -144,4 +189,19 @@ public class WorkflowService {
         response = mapper.convertValue(optional, ProcessInstanceResponse.class);
         return response.getProcessInstances().get(0).getState();
     }
+
+
+    private StringBuilder getprocessInstanceSearchURL(String tenantId, String serviceRequestId) {
+
+        StringBuilder url = new StringBuilder(pgrConfiguration.getWfHost());
+        url.append(pgrConfiguration.getWfProcessInstanceSearchPath());
+        url.append("?tenantId=");
+        url.append(tenantId);
+        url.append("&businessIds=");
+        url.append(serviceRequestId);
+        return url;
+
+    }
+
+
 }
