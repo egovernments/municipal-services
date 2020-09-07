c9e69dc38a35a7b02fe15189029a4b44587d83c7
Rajanihn <rajnihn@gmail.com>
2020-09-07 15:50:27 +0530

    Added receipt listener for challan

---
+++ echallan-services/src/main/java/org/egov/echallan/consumer/ReceiptConsumer.java
@@ -0,0 +1,31 @@
+package org.egov.echallan.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.egov.echallan.service.PaymentUpdateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+import java.util.HashMap;
+
+
+
+@Slf4j
+@Component
+public class ReceiptConsumer {
+
+    private PaymentUpdateService paymentUpdateService;
+
+    @Autowired
+    public ReceiptConsumer(PaymentUpdateService paymentUpdateService) {
+        this.paymentUpdateService = paymentUpdateService;
+    }
+
+    @KafkaListener(topics = {"${kafka.topics.receipt.create}"})
+    public void listen(final HashMap<String, Object> record, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        
+        paymentUpdateService.process(record);
+    }
+}
---
+++ echallan-services/src/main/java/org/egov/echallan/service/PaymentUpdateService.java
@@ -0,0 +1,73 @@
+package org.egov.echallan.service;
+
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.egov.common.contract.request.RequestInfo;
+import org.egov.echallan.config.ChallanConfiguration;
+import org.egov.echallan.model.AuditDetails;
+import org.egov.echallan.model.Challan;
+import org.egov.echallan.model.ChallanRequest;
+import org.egov.echallan.model.SearchCriteria;
+import org.egov.echallan.producer.Producer;
+import org.egov.echallan.util.CommonUtils;
+import org.egov.echallan.web.models.collection.PaymentDetail;
+import org.egov.echallan.web.models.collection.PaymentRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class PaymentUpdateService {
+	
+	@Autowired
+	private ObjectMapper mapper; 
+	
+	@Autowired
+	private ChallanService challanService;
+	
+	@Autowired
+	private Producer producer;
+	
+	@Autowired
+	private ChallanConfiguration config;
+	
+	@Autowired
+	 private CommonUtils commUtils;
+	
+	public static final String STATUS_PAID  = "PAID";
+	
+	
+	public void process(HashMap<String, Object> record) {
+
+		try {
+
+			PaymentRequest paymentRequest = mapper.convertValue(record, PaymentRequest.class);
+			RequestInfo requestInfo = paymentRequest.getRequestInfo();
+
+			List<PaymentDetail> paymentDetails = paymentRequest.getPayment().getPaymentDetails();
+			String tenantId = paymentRequest.getPayment().getTenantId();
+
+			for (PaymentDetail paymentDetail : paymentDetails) {
+				SearchCriteria criteria = new SearchCriteria();
+				criteria.setTenantId(tenantId);
+				criteria.setChallanNo(paymentDetail.getBill().getConsumerCode());
+				criteria.setBusinessService(paymentDetail.getBusinessService());
+				List<Challan> challans = challanService.search(criteria, requestInfo);
+				String uuid = requestInfo.getUserInfo().getUuid();
+			    AuditDetails auditDetails = commUtils.getAuditDetails(uuid, true);
+				challans.forEach(challan -> challan.setApplicationStatus(STATUS_PAID));
+				challans.get(0).setAuditDetails(auditDetails);
+				ChallanRequest request = ChallanRequest.builder().requestInfo(requestInfo).challan(challans.get(0)).build();
+				producer.push(config.getUpdateChallanTopic(), request);
+				
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
