a25f3c980477b36287829ecdeb0e24227501fe64
rohit-eGov <rohit.shende@egovernments.org>
2020-10-01 15:30:00 +0530

    RAIN-1543: Removed unnecessary index from tl-calculator

---
+++ tl-calculator/src/main/resources/db/migration/main/V20201001150339__drop_unnecessary_tl_billingslab_index.sql
@@ -0,0 +1,11 @@
+DROP INDEX IF EXISTS index_eg_tl_billingslab_tenantid;
+
+DROP INDEX IF EXISTS index_eg_tl_billingslab_accessorycategory;
+
+DROP INDEX IF EXISTS index_eg_tl_billingslab_tradetype;
+
+DROP INDEX IF EXISTS index_eg_tl_billingslab_fromUom;
+
+DROP INDEX IF EXISTS index_eg_tl_billingslab_toUom;
+
+DROP INDEX IF EXISTS index_eg_tl_billingslab_uom;
