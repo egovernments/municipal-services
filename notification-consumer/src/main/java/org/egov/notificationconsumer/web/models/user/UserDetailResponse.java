b16cdbaf110e351a288b49222257059b41b33dc9
praneeth-egov <praneeth.reddy@egovernments.org>
2020-10-28 16:11:31 +0530

    Notification Consumer Module

---
+++ notification-consumer/src/main/java/org/egov/notificationconsumer/web/models/user/UserDetailResponse.java
@@ -0,0 +1,20 @@
+package org.egov.notificationconsumer.web.models.user;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.egov.common.contract.response.ResponseInfo;
+import org.egov.notificationconsumer.web.models.User;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserDetailResponse {
+    @JsonProperty("responseInfo")
+    ResponseInfo responseInfo;
+
+    @JsonProperty("user")
+    List<User> user;
+}
