0a45564683f55a95fa760aa8e1604d2727aff1d3
vipin <vipinpublicdomain@gmail.com>
2020-11-12 11:44:39 +0530

    Validation on challan update

--- echallan-services/src/main/java/org/egov/echallan/service/PaymentUpdateService.java
+++ echallan-services/src/main/java/org/egov/echallan/service/PaymentUpdateService.java
@@ -3,7 +3,6 @@ package org.egov.echallan.service;
 
 import java.util.HashMap;
 import java.util.List;
-
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.echallan.config.ChallanConfiguration;
 import org.egov.echallan.model.AuditDetails;
@@ -13,16 +12,20 @@ import org.egov.echallan.model.ChallanRequest;
 import org.egov.echallan.model.SearchCriteria;
 import org.egov.echallan.producer.Producer;
 import org.egov.echallan.util.CommonUtils;
+import org.egov.echallan.web.models.collection.Payment;
 import org.egov.echallan.web.models.collection.PaymentDetail;
 import org.egov.echallan.web.models.collection.PaymentRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static org.egov.echallan.util.ChallanConstants.*;
+import lombok.extern.slf4j.Slf4j;
+ 
 
 @Service
+@Slf4j
 public class PaymentUpdateService {
 	
 	@Autowired
@@ -45,29 +48,31 @@ public class PaymentUpdateService {
 	public void process(HashMap<String, Object> record) {
 
 		try {
-
+			log.info("Process for object", record);
 			PaymentRequest paymentRequest = mapper.convertValue(record, PaymentRequest.class);
 			RequestInfo requestInfo = paymentRequest.getRequestInfo();
-
+			//Update the challan only when the payment is fully done.
+			if(!paymentRequest.getPayment().getTotalAmountPaid().equals(paymentRequest.getPayment().getTotalDue())) 
+				return;
 			List<PaymentDetail> paymentDetails = paymentRequest.getPayment().getPaymentDetails();
-			String tenantId = paymentRequest.getPayment().getTenantId();
-
 			for (PaymentDetail paymentDetail : paymentDetails) {
 				SearchCriteria criteria = new SearchCriteria();
-				criteria.setTenantId(tenantId);
+				criteria.setTenantId(paymentRequest.getPayment().getTenantId());
 				criteria.setChallanNo(paymentDetail.getBill().getConsumerCode());
 				criteria.setBusinessService(paymentDetail.getBusinessService());
 				List<Challan> challans = challanService.search(criteria, requestInfo);
-				String uuid = requestInfo.getUserInfo().getUuid();
-			    AuditDetails auditDetails = commUtils.getAuditDetails(uuid, true);
-				challans.forEach(challan -> challan.setApplicationStatus(StatusEnum.PAID));
-				challans.get(0).setAuditDetails(auditDetails);
-				ChallanRequest request = ChallanRequest.builder().requestInfo(requestInfo).challan(challans.get(0)).build();
-				producer.push(config.getUpdateChallanTopic(), request);
-				
+				//update challan only if payment is done for challan. 
+				if(!CollectionUtils.isEmpty(challans) ) {
+					String uuid = requestInfo.getUserInfo().getUuid();
+				    AuditDetails auditDetails = commUtils.getAuditDetails(uuid, true);
+					challans.forEach(challan -> challan.setApplicationStatus(StatusEnum.PAID));
+					challans.get(0).setAuditDetails(auditDetails);
+					ChallanRequest request = ChallanRequest.builder().requestInfo(requestInfo).challan(challans.get(0)).build();
+					producer.push(config.getUpdateChallanTopic(), request);
+				}
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
+			log.error("Exception while processing payment update: ",e);
 		}
 
 	}
