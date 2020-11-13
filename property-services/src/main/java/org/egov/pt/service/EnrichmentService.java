707d6df96d97d9131924f3d4ca190bfc24bdac34
kavi_elrey@1993 <25226238+kavi-egov@users.noreply.github.com>
2020-11-13 18:43:32 +0530

    Update EnrichmentService.java (#516)

--- property-services/src/main/java/org/egov/pt/service/EnrichmentService.java
+++ property-services/src/main/java/org/egov/pt/service/EnrichmentService.java
@@ -104,7 +104,7 @@ public class EnrichmentService {
     	
     	Property property = request.getProperty();
         RequestInfo requestInfo = request.getRequestInfo();
-        AuditDetails auditDetailsForUpdate = propertyutil.getAuditDetails(requestInfo.getUserInfo().getUuid().toString(), false);
+        AuditDetails auditDetailsForUpdate = propertyutil.getAuditDetails(requestInfo.getUserInfo().getUuid().toString(), true);
         propertyFromDb.setAuditDetails(auditDetailsForUpdate);
         
         
@@ -212,7 +212,7 @@ public class EnrichmentService {
 		Property property = request.getProperty();
 		Boolean isWfEnabled = config.getIsMutationWorkflowEnabled();
 		Boolean iswfStarting = propertyFromSearch.getStatus().equals(Status.ACTIVE);
-		AuditDetails auditDetailsForUpdate = propertyutil.getAuditDetails(requestInfo.getUserInfo().getUuid().toString(), false);
+		AuditDetails auditDetailsForUpdate = propertyutil.getAuditDetails(requestInfo.getUserInfo().getUuid().toString(), true);
 		propertyFromSearch.setAuditDetails(auditDetailsForUpdate);
 
 		if (!isWfEnabled) {
