33a151165d4d63505fc117de533f0b4749bc1776
Tarun Lalwani <tarunlalwani@gmail.com>
2020-11-16 17:05:59 +0530

    Add caching for billing slabs

--- pt-calculator-v2/src/main/java/org/egov/pt/calculator/PropertyCalculatorApplication.java
+++ pt-calculator-v2/src/main/java/org/egov/pt/calculator/PropertyCalculatorApplication.java
@@ -3,6 +3,7 @@ package org.egov.pt.calculator;
 import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
@@ -13,6 +14,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 @SpringBootApplication
 @Import({ TracerConfiguration.class })
+@EnableCaching
 public class PropertyCalculatorApplication {
 
 	public static void main(String[] args) {
