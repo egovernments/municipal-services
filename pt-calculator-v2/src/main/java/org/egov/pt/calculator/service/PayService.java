df3d9ba7a739a0c995352c58000ffa745f59121e
narendrabandhamneni-wt-egov <narendra.bandhamaneni@egovernments.org>
2021-01-08 19:28:15 +0530

    reverted changes

--- pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/PayService.java
+++ pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/PayService.java
@@ -263,26 +263,32 @@ public class PayService {
 					interestAmt = interestAmt.add(interestCalculated);
 				} else {
 
-					// we need consider what ever payment related finanical year which is iterating
-					// currently
-					Payment currentFinanicalPayment = null;
-					for (Payment paymentObject : filteredPaymentsAfterIntersetDate) {
 
-						for (PaymentDetail paymentDetail : paymentObject.getPaymentDetails()) {
-
-							for (BillDetail billDetails : paymentDetail.getBill().getBillDetails()) {
-
-								if (billDetails.getFromPeriod().equals(taxPeriod.getFromDate())
-										&& billDetails.getToPeriod().equals(taxPeriod.getToDate())) {
-									currentFinanicalPayment = paymentObject;
-
+                   // we need consider what ever  payment related finanical year which is iterating currently
+					Payment currentFinanicalPayment=null;
+					for(Payment paymentObject : filteredPaymentsAfterIntersetDate) {
+						
+						for(PaymentDetail paymentDetail: paymentObject.getPaymentDetails()) {
+							
+							for(BillDetail billDetails: paymentDetail.getBill().getBillDetails()) {
+								
+								if(billDetails.getFromPeriod().equals(taxPeriod.getFromDate())&& billDetails.getToPeriod().equals(taxPeriod.getToDate())) {
+									currentFinanicalPayment=paymentObject;
+									break;
 								}
 							}
-
+							
+							if(currentFinanicalPayment!=null) {
+								break;
+							}
 						}
-
+						if(currentFinanicalPayment!=null) {
+							break;
+						}
+						
 					}
-
+					
+					
 					for (int i = 0; i < numberOfPeriods; i++) {
 
 						if (i != numberOfPeriods - 1)
@@ -298,22 +304,20 @@ public class PayService {
 							interestCalculated = calculateInterest(numberOfDaysInMillies, applicableAmount,
 									interestMap);
 						} else if (i == numberOfPeriods - 1) {
-							if (currentFinanicalPayment == null) {
-								currentFinanicalPayment = payment;
-							}
-
-							// we need to pass current iterating financial year payment for getting
-							// applicable amount.
-							applicableAmount = utils
-									.getTaxAmtFromPaymentForApplicablesGeneration(currentFinanicalPayment, taxPeriod);
+                           if(currentFinanicalPayment==null) {
+                        	   currentFinanicalPayment=payment;
+                           }
+                           
+                           // we need to pass current iterating financial year payment for getting applicable amount.
+							applicableAmount = utils.getTaxAmtFromPaymentForApplicablesGeneration(currentFinanicalPayment, taxPeriod);
 							numberOfDaysInMillies = getEODEpoch(currentUTC) - getEODEpoch(payment.getTransactionDate());
 							interestCalculated = calculateInterest(numberOfDaysInMillies, applicableAmount,
 									interestMap);
 						} else {
 
 							Payment paymentPrev = filteredPaymentsAfterIntersetDate.get(i - 1);
-							applicableAmount = utils
-									.getTaxAmtFromPaymentForApplicablesGeneration(currentFinanicalPayment, taxPeriod);
+							applicableAmount = utils.getTaxAmtFromPaymentForApplicablesGeneration(currentFinanicalPayment,
+									taxPeriod);
 							numberOfDaysInMillies = getEODEpoch(payment.getTransactionDate())
 									- getEODEpoch(paymentPrev.getTransactionDate());
 							interestCalculated = calculateInterest(numberOfDaysInMillies, applicableAmount,
