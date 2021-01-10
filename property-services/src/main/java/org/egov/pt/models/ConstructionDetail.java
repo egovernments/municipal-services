ff8bfe29acb2a1d98950f2a8ecbf19e3e1a64dc1
kaviyarasan1993 <kaviyarasanit@gmail.com>
2021-01-10 18:04:32 +0530

    adding notnull to builtup area in unit

--- property-services/src/main/java/org/egov/pt/models/ConstructionDetail.java
+++ property-services/src/main/java/org/egov/pt/models/ConstructionDetail.java
@@ -3,6 +3,7 @@ package org.egov.pt.models;
 import java.math.BigDecimal;
 
 import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -33,6 +34,7 @@ public class ConstructionDetail {
 	private BigDecimal carpetArea;
 
 	@Digits(integer = 8, fraction = 2)
+	@NotNull
 	@JsonProperty("builtUpArea")
 	private BigDecimal builtUpArea;
 
