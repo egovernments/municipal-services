4d9822144235cf8a8687a58c763ef713cbb62c71
rohit-eGov <rohit.shende@egovernments.org>
2021-02-03 15:11:06 +0530

    RAIN-2154 Added additionalDetails column in sewerage service

--- sw-services/src/main/java/org/egov/swservice/service/EnrichmentService.java
+++ sw-services/src/main/java/org/egov/swservice/service/EnrichmentService.java
@@ -90,12 +90,12 @@ public class EnrichmentService {
 					sewerageConnectionRequest.getSewerageConnection().getAdditionalDetails(), HashMap.class);
 			List<String> adhocPenalityAndRebateConst = Arrays.asList(SWConstants.ADHOC_PENALTY,
 					SWConstants.ADHOC_REBATE, SWConstants.APP_CREATED_DATE, SWConstants.ESTIMATION_DATE_CONST);
-			for (Map.Entry<String, Object> entrySet: addDetail.entrySet()) {
-				if (addDetail.getOrDefault(entrySet.getKey(), null) != null && adhocPenalityAndRebateConst.contains(entrySet.getKey())) {
-					BigDecimal big = new BigDecimal(String.valueOf(addDetail.get(entrySet.getKey())));
-					additionalDetail.put(entrySet.getKey(), big);
+			for (String constKey : SWConstants.ADDITIONAL_OBJECT) {
+				if (addDetail.getOrDefault(constKey, null) != null && adhocPenalityAndRebateConst.contains(constKey)) {
+					BigDecimal big = new BigDecimal(String.valueOf(addDetail.get(constKey)));
+					additionalDetail.put(constKey, big);
 				} else {
-					additionalDetail.put(entrySet.getKey(), addDetail.get(entrySet.getKey()));
+					additionalDetail.put(constKey, addDetail.get(constKey));
 				}
 			}
 			if (sewerageConnectionRequest.getSewerageConnection().getProcessInstance().getAction()
