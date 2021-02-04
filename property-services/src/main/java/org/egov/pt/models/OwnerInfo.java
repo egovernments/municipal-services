2e67756b235aeffdff2f334e0002a6276a39e58a
Phani Kiran Gutha <phani.kiran@walkingtree.tech>
2021-02-04 11:56:25 +0530

    Develop Patch to sync with develop branch

--- property-services/src/main/java/org/egov/pt/models/OwnerInfo.java
+++ property-services/src/main/java/org/egov/pt/models/OwnerInfo.java
@@ -19,7 +19,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
@@ -266,8 +265,4 @@ public class OwnerInfo extends User {
 				+ getCreatedDate() + ", LastModifiedBy=" + getLastModifiedBy() + ", LastModifiedDate="
 				+ getLastModifiedDate() + ", TenantId=" + getTenantId() + "]";
 	}
-
-
-
-
 }
