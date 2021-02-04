8a963d3ef902c8d4a4a6d173dd6644ed9e18c2ae
egovmis <mis.pmidc@punjab.gov.in>
2021-02-04 10:22:52 +0530

    changed column name operatioal area

--- tl-services/src/main/resources/db/migration/main/V20210204100630__tl_alter_operational_area_ddl5.sql
+++ tl-services/src/main/resources/db/migration/main/V20210204100630__tl_alter_operational_area_ddl5.sql
@@ -1,1 +1,1 @@
-ALTER TABLE eg_tl_tradelicensedetail ALTER COLUMN operationalArea TYPE character varying(64);
+ALTER TABLE eg_tl_tradelicensedetail ALTER COLUMN operationalarea TYPE character varying(64);
