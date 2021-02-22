bc15db652a21cc02c98a75dad1e820de1f5c6ae3
vipin <vipinpublicdomain@gmail.com>
2021-02-22 17:47:45 +0530

    sync with challan service

--- echallan-calculator/src/main/java/org/egov/echallancalculation/model/Address.java
+++ echallan-calculator/src/main/java/org/egov/echallancalculation/model/Address.java
@@ -12,18 +12,14 @@
 
 package org.egov.echallancalculation.model;
 
-import java.util.List;
-import java.util.Objects;
-
 import javax.validation.Valid;
 import javax.validation.constraints.Size;
 
-import org.egov.common.contract.response.ResponseInfo;
+import org.springframework.validation.annotation.Validated;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 
+import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -33,374 +29,81 @@ import lombok.Setter;
 /**
  * Representation of a address. Indiavidual APIs may choose to extend from this using allOf if more details needed to be added in their case. 
  */
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2020-08-10T16:46:24.044+05:30[Asia/Calcutta]")
+@ApiModel(description = "Representation of a address. Indiavidual APIs may choose to extend from this using allOf if more details needed to be added in their case. ")
+@Validated
+@javax.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2018-09-18T17:06:11.263+05:30")
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Address {
+public class Address   {
 
-  @JsonProperty("tenantId")
+        @Size(max=64)
+        @JsonProperty("id")
+        private String id;
 
-  private String tenantId = null;
+        @Size(max=64)
+        @JsonProperty("tenantId")
+        private String tenantId = null;
 
-  @JsonProperty("doorNo")
+        @Size(max=64)
+        @JsonProperty("doorNo")
+        private String doorNo = null;
 
-  private String doorNo = null;
+        @JsonProperty("latitude")
+        private Double latitude = null;
 
-  @JsonProperty("plotNo")
+        @JsonProperty("longitude")
+        private Double longitude = null;
 
-  private String plotNo = null;
+        @Size(max=64)
+        @JsonProperty("addressId")
+        private String addressId = null;
 
-  @JsonProperty("id")
+        @Size(max=64)
+        @JsonProperty("addressNumber")
+        private String addressNumber = null;
 
-  private String id = null;
+        @Size(max=64)
+        @JsonProperty("type")
+        private String type = null;
 
-  @JsonProperty("landmark")
+        @JsonProperty("addressLine1")
+        private String addressLine1 = null;
 
-  private String landmark = null;
-  
-  @JsonProperty("latitude")
-  private Double latitude = null;
+        @Size(max=256)
+        @JsonProperty("addressLine2")
+        private String addressLine2 = null;
 
-  @JsonProperty("longitude")
-  private Double longitude = null;
+        @Size(max=64)
+        @JsonProperty("landmark")
+        private String landmark = null;
 
-  @JsonProperty("city")
+        @Size(max=64)
+        @JsonProperty("city")
+        private String city = null;
 
-  private String city = null;
+        @Size(max=64)
+        @JsonProperty("pincode")
+        private String pincode = null;
 
-  @JsonProperty("district")
+        @Size(max=64)
+        @JsonProperty("detail")
+        private String detail = null;
 
-  private String district = null;
+        @Size(max=64)
+        @JsonProperty("buildingName")
+        private String buildingName = null;
 
-  @JsonProperty("region")
+        @Size(max=64)
+        @JsonProperty("street")
+        private String street = null;
 
-  private String region = null;
+        @Valid
+        @JsonProperty("locality")
+        private Boundary locality = null;
 
-  @JsonProperty("state")
-
-  private String state = null;
-
-  @JsonProperty("country")
-
-  private String country = null;
-
-  @JsonProperty("pincode")
-
-  private String pincode = null;
-
-  @JsonProperty("additionDetails")
-
-  private String additionDetails = null;
-
-  @JsonProperty("buildingName")
-
-  private String buildingName = null;
-
-  @JsonProperty("street")
-
-  private String street = null;
-
-  @Valid
-  @JsonProperty("locality")
-  private Boundary locality = null;
-  
-  @Size(max=64)
-  @JsonProperty("detail")
-  private String detail = null;
-
-  /**
-  * Unique Identifier of the tenant to which user primarily belongs
-  * @return tenantId
-  **/
-  public String getTenantId() {
-    return tenantId;
-  }
-  public void setTenantId(String tenantId) {
-    this.tenantId = tenantId;
-  }
-  public Address doorNo(String doorNo) {
-    this.doorNo = doorNo;
-    return this;
-  }
-
-  
-
-  /**
-  * House number or door number.
-  * @return doorNo
-  **/
-  public String getDoorNo() {
-    return doorNo;
-  }
-  public void setDoorNo(String doorNo) {
-    this.doorNo = doorNo;
-  }
-  public Address plotNo(String plotNo) {
-    this.plotNo = plotNo;
-    return this;
-  }
-
-  
-
-  /**
-  * Plot number of the house.
-  * @return plotNo
-  **/
-  public String getPlotNo() {
-    return plotNo;
-  }
-  public void setPlotNo(String plotNo) {
-    this.plotNo = plotNo;
-  }
-  /**
-  * System generated id for the address
-  * @return id
-  **/
-  public String getId() {
-    return id;
-  }
-  public Address landmark(String landmark) {
-    this.landmark = landmark;
-    return this;
-  }
-
-  
-
-  /**
-  * additional landmark to help locate the address
-  * @return landmark
-  **/
-  public String getLandmark() {
-    return landmark;
-  }
-  public void setLandmark(String landmark) {
-    this.landmark = landmark;
-  }
-  public Address city(String city) {
-    this.city = city;
-    return this;
-  }
-
-  
-
-  /**
-  * City of the address. Can be represented by the tenantid itself
-  * @return city
-  **/
-  public String getCity() {
-    return city;
-  }
-  public void setCity(String city) {
-    this.city = city;
-  }
-  public Address district(String district) {
-    this.district = district;
-    return this;
-  }
-
-  
-
-  /**
-  * The district in which the property is located
-  * @return district
-  **/
-  public String getDistrict() {
-    return district;
-  }
-  public void setDistrict(String district) {
-    this.district = district;
-  }
-  public Address region(String region) {
-    this.region = region;
-    return this;
-  }
-
-  
-
-  /**
-  * The Region in which the property is located
-  * @return region
-  **/
-  public String getRegion() {
-    return region;
-  }
-  public void setRegion(String region) {
-    this.region = region;
-  }
-  public Address state(String state) {
-    this.state = state;
-    return this;
-  }
-
-  
-
-  /**
-  * The State in which the property is located
-  * @return state
-  **/
-  public String getState() {
-    return state;
-  }
-  public void setState(String state) {
-    this.state = state;
-  }
-  public Address country(String country) {
-    this.country = country;
-    return this;
-  }
-
-  
-
-  /**
-  * The country in which the property is located
-  * @return country
-  **/
-  public String getCountry() {
-    return country;
-  }
-  public void setCountry(String country) {
-    this.country = country;
-  }
-  public Address pincode(String pincode) {
-    this.pincode = pincode;
-    return this;
-  }
-
-  
-
-  /**
-  * PIN code of the address. Indian pincodes will usually be all numbers.
-  * @return pincode
-  **/
-  public String getPincode() {
-    return pincode;
-  }
-  public void setPincode(String pincode) {
-    this.pincode = pincode;
-  }
-  public Address additionDetails(String additionDetails) {
-    this.additionDetails = additionDetails;
-    return this;
-  }
-
-  
-
-  /**
-  * more address detail as may be needed
-  * @return additionDetails
-  **/
-  public String getAdditionDetails() {
-    return additionDetails;
-  }
-  public void setAdditionDetails(String additionDetails) {
-    this.additionDetails = additionDetails;
-  }
-  public Address buildingName(String buildingName) {
-    this.buildingName = buildingName;
-    return this;
-  }
-
-  
-
-  /**
-  * Name of the building
-  * @return buildingName
-  **/
-  public String getBuildingName() {
-    return buildingName;
-  }
-  public void setBuildingName(String buildingName) {
-    this.buildingName = buildingName;
-  }
-  public Address street(String street) {
-    this.street = street;
-    return this;
-  }
-
-  
-
-  /**
-  * Street Name
-  * @return street
-  **/
-  public String getStreet() {
-    return street;
-  }
-  public void setStreet(String street) {
-    this.street = street;
-  }
-  
-
-  
-
-  
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Address address = (Address) o;
-    return Objects.equals(this.tenantId, address.tenantId) &&
-        Objects.equals(this.doorNo, address.doorNo) &&
-        Objects.equals(this.plotNo, address.plotNo) &&
-        Objects.equals(this.id, address.id) &&
-        Objects.equals(this.landmark, address.landmark) &&
-        Objects.equals(this.city, address.city) &&
-        Objects.equals(this.district, address.district) &&
-        Objects.equals(this.region, address.region) &&
-        Objects.equals(this.state, address.state) &&
-        Objects.equals(this.country, address.country) &&
-        Objects.equals(this.pincode, address.pincode) &&
-        Objects.equals(this.additionDetails, address.additionDetails) &&
-        Objects.equals(this.buildingName, address.buildingName) &&
-        Objects.equals(this.street, address.street) ;
-       
-  }
-
-  @Override
-  public int hashCode() {
-    return java.util.Objects.hash(tenantId, doorNo, plotNo, id, landmark, city, district, region, state, country, pincode, additionDetails, buildingName, street);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class Address {\n");
-    
-    sb.append("    tenantId: ").append(toIndentedString(tenantId)).append("\n");
-    sb.append("    doorNo: ").append(toIndentedString(doorNo)).append("\n");
-    sb.append("    plotNo: ").append(toIndentedString(plotNo)).append("\n");
-    sb.append("    id: ").append(toIndentedString(id)).append("\n");
-    sb.append("    landmark: ").append(toIndentedString(landmark)).append("\n");
-    sb.append("    city: ").append(toIndentedString(city)).append("\n");
-    sb.append("    district: ").append(toIndentedString(district)).append("\n");
-    sb.append("    region: ").append(toIndentedString(region)).append("\n");
-    sb.append("    state: ").append(toIndentedString(state)).append("\n");
-    sb.append("    country: ").append(toIndentedString(country)).append("\n");
-    sb.append("    pincode: ").append(toIndentedString(pincode)).append("\n");
-    sb.append("    additionDetails: ").append(toIndentedString(additionDetails)).append("\n");
-    sb.append("    buildingName: ").append(toIndentedString(buildingName)).append("\n");
-    sb.append("    street: ").append(toIndentedString(street)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
 
 }
--- echallan-calculator/src/main/java/org/egov/echallancalculation/model/Boundary.java
+++ echallan-calculator/src/main/java/org/egov/echallancalculation/model/Boundary.java
@@ -11,226 +11,64 @@
  */
 
 package org.egov.echallancalculation.model;
-
-import java.util.Objects;
-
-import org.egov.common.contract.response.ResponseInfo;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Builder;
 
 /**
  * Boundary
  */
+@Validated
+@javax.annotation.Generated(value = "org.egov.codegen.SpringBootCodegen", date = "2020-07-15T11:35:33.568+05:30")
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaClientCodegen", date = "2020-08-10T16:46:24.044+05:30[Asia/Calcutta]")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Boundary {
+public class Boundary   {
 
-  @JsonProperty("code")
+        @NotNull
+        @JsonProperty("code")
+        private String code = null;
 
-  private String code = null;
+        @JsonProperty("name")
+        private String name = null;
 
-  @JsonProperty("name")
+        @JsonProperty("label")
+        private String label = null;
 
-  private String name = null;
+        @JsonProperty("latitude")
+        private String latitude = null;
 
-  @JsonProperty("label")
+        @JsonProperty("longitude")
+        private String longitude = null;
 
-  private String label = null;
+        @JsonProperty("children")
+        @Valid
+        private List<Boundary> children = null;
 
-  @JsonProperty("latitude")
+        @JsonProperty("materializedPath")
+        private String materializedPath = null;
 
-  private String latitude = null;
 
-  @JsonProperty("longitude")
-
-  private String longitude = null;
-
-  @JsonProperty("children")
-
-  private List<Boundary> children = null;
-
-  @JsonProperty("materializedPath")
-
-  private String materializedPath = null;
-  public Boundary code(String code) {
-    this.code = code;
-    return this;
-  }
-
-  
-
-  /**
-  * code of the boundary.
-  * @return code
-  **/
-  public String getCode() {
-    return code;
-  }
-  public void setCode(String code) {
-    this.code = code;
-  }
-  public Boundary name(String name) {
-    this.name = name;
-    return this;
-  }
-
-  
-
-  /**
-  * name of the boundary.
-  * @return name
-  **/
-  public String getName() {
-    return name;
-  }
-  public void setName(String name) {
-    this.name = name;
-  }
-  public Boundary label(String label) {
-    this.label = label;
-    return this;
-  }
-
-  
-
-  /**
-  * localized label for the boundry.
-  * @return label
-  **/
-  public String getLabel() {
-    return label;
-  }
-  public void setLabel(String label) {
-    this.label = label;
-  }
-  public Boundary latitude(String latitude) {
-    this.latitude = latitude;
-    return this;
-  }
-
-  
-
-  /**
-  * latitude of the boundary.
-  * @return latitude
-  **/
-  public String getLatitude() {
-    return latitude;
-  }
-  public void setLatitude(String latitude) {
-    this.latitude = latitude;
-  }
-  public Boundary longitude(String longitude) {
-    this.longitude = longitude;
-    return this;
-  }
-
-  
-
-  /**
-  * longitude of the boundary.
-  * @return longitude
-  **/
-  public String getLongitude() {
-    return longitude;
-  }
-  public void setLongitude(String longitude) {
-    this.longitude = longitude;
-  }
-  public Boundary children(List<Boundary> children) {
-    this.children = children;
-    return this;
-  }
-
-  public Boundary addChildrenItem(Boundary childrenItem) {
-    if (this.children == null) {
-      this.children = new ArrayList<Boundary>();
-    }
-    this.children.add(childrenItem);
-    return this;
-  }
-
-  /**
-  * Get children
-  * @return children
-  **/
-  public List<Boundary> getChildren() {
-    return children;
-  }
-  public void setChildren(List<Boundary> children) {
-    this.children = children;
-  }
-  /**
-  * materialized path of the boundary - this would be of the format tenantid.[code] from parentt till teh current boundary
-  * @return materializedPath
-  **/
-  public String getMaterializedPath() {
-    return materializedPath;
-  }
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Boundary boundary = (Boundary) o;
-    return Objects.equals(this.code, boundary.code) &&
-        Objects.equals(this.name, boundary.name) &&
-        Objects.equals(this.label, boundary.label) &&
-        Objects.equals(this.latitude, boundary.latitude) &&
-        Objects.equals(this.longitude, boundary.longitude) &&
-        Objects.equals(this.children, boundary.children) &&
-        Objects.equals(this.materializedPath, boundary.materializedPath);
-  }
-
-  @Override
-  public int hashCode() {
-    return java.util.Objects.hash(code, name, label, latitude, longitude, children, materializedPath);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class Boundary {\n");
-    
-    sb.append("    code: ").append(toIndentedString(code)).append("\n");
-    sb.append("    name: ").append(toIndentedString(name)).append("\n");
-    sb.append("    label: ").append(toIndentedString(label)).append("\n");
-    sb.append("    latitude: ").append(toIndentedString(latitude)).append("\n");
-    sb.append("    longitude: ").append(toIndentedString(longitude)).append("\n");
-    sb.append("    children: ").append(toIndentedString(children)).append("\n");
-    sb.append("    materializedPath: ").append(toIndentedString(materializedPath)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
-  }
+        public Boundary addChildrenItem(Boundary childrenItem) {
+            if (this.children == null) {
+            this.children = new ArrayList<>();
+            }
+        this.children.add(childrenItem);
+        return this;
+        }
 
 }
+
