9ecef0acd9ffcb346cf4eba388e9bc4c507b5dda
vipin <vipinpublicdomain@gmail.com>
2021-02-22 14:39:51 +0530

    Update Address.java

--- echallan-services/src/main/java/org/egov/echallan/model/Address.java
+++ echallan-services/src/main/java/org/egov/echallan/model/Address.java
@@ -12,19 +12,19 @@
 
 package org.egov.echallan.model;
 
-import java.util.Objects;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonCreator;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import org.springframework.validation.annotation.Validated;
 import javax.validation.Valid;
-import javax.validation.constraints.*;
+import javax.validation.constraints.Size;
+
+import org.springframework.validation.annotation.Validated;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.Builder;
 
 /**
  * Representation of a address. Indiavidual APIs may choose to extend from this using allOf if more details needed to be added in their case. 
