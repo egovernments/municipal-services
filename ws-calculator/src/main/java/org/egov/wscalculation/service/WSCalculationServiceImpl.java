6f6465b4f64e06d2ed6147e166103aaf31f02dea
mahammadnizam-wt-eGov <mahammad.nizam@egovernments.org>
2021-03-14 18:08:17 +0530

    [PI-1251]-Added tax period from and tax period to for water CalculationCriteria

--- ws-calculator/src/main/java/org/egov/wscalculation/service/WSCalculationServiceImpl.java
+++ ws-calculator/src/main/java/org/egov/wscalculation/service/WSCalculationServiceImpl.java
@@ -223,6 +223,10 @@ public class WSCalculationServiceImpl implements WSCalculationService {
 	List<Calculation> getCalculations(CalculationReq request, Map<String, Object> masterMap) {
 		List<Calculation> calculations = new ArrayList<>(request.getCalculationCriteria().size());
 		for (CalculationCriteria criteria : request.getCalculationCriteria()) {
+			if(criteria.getFrom() <= 0 || criteria.getTo() <= 0){
+				criteria.setFrom(request.getTaxPeriodFrom());
+				criteria.setTo(request.getTaxPeriodTo());
+			}
 			Map<String, List> estimationMap = estimationService.getEstimationMap(criteria, request,
 					masterMap);
 			ArrayList<?> billingFrequencyMap = (ArrayList<?>) masterMap
