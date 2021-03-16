92cdc46a0e9f51e5cd7fad3fde732ff173e01f49
narendrabandhamneni-wt-egov <narendra.bandhamaneni@egovernments.org>
2021-03-16 12:47:36 +0530

    modified validation for water migration

--- property-services/src/main/java/org/egov/pt/validator/PropertyValidator.java
+++ property-services/src/main/java/org/egov/pt/validator/PropertyValidator.java
@@ -630,7 +630,7 @@ public class PropertyValidator {
 			return false;
 		else if (mobileNumber.length() != 10)
 			return false;
-		else if (Character.getNumericValue(mobileNumber.charAt(0)) < 5)
+		else if (Character.getNumericValue(mobileNumber.charAt(0)) < 4)
 			return false;
 		else
 			return true;
