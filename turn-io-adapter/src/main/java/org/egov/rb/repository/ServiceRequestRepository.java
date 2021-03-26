1c55ec2d1811deaf50028fb22fc1c75bcecd843f
narendrabandhamneni-wt-egov <narendra.bandhamaneni@egovernments.org>
2021-03-26 19:47:21 +0530

    Integration with pgr

--- turn-io-adapter/src/main/java/org/egov/rb/repository/ServiceRequestRepository.java
+++ turn-io-adapter/src/main/java/org/egov/rb/repository/ServiceRequestRepository.java
@@ -6,10 +6,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.egov.rb.contract.ServiceRequest;
-import org.egov.rb.contract.ServiceResponse;
-import org.egov.rb.model.MessageRequest;
-import org.egov.rb.user.models.RequestInfo;
+import org.egov.rb.pgrmodels.RequestInfo;
+import org.egov.rb.pgrmodels.ServiceRequest;
+import org.egov.rb.pgrmodels.ServiceResponse;
 import org.egov.tracer.model.ServiceCallException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,70 +33,28 @@ import org.springframework.web.client.RestTemplate;
 public class ServiceRequestRepository {
 
 	
-	@Value("${egov.pgr.host}")
-	private String userHost;
-	
-	@Value("${egov.pgr.create.endpoint}")
-	private String userCreateEndPoint;
-	
+
 	@Autowired
 	private RestTemplate restTemplate;
 	
 
-	  public Object fetchResult(StringBuilder uri, ServiceRequest serviceRequest) {
-		  RestTemplate template=new RestTemplate();
-		 
-		  StringBuilder uri1 = new StringBuilder();
-		  Object response=null; 
-		  ObjectMapper mapper = new ObjectMapper();
+	  public ServiceResponse fetchResult(StringBuilder uri, ServiceRequest serviceRequest) {
 	  
+		  ServiceResponse serviceResponse=null;
 	  try {
-		  HttpHeaders headers= new HttpHeaders();
-		  headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		  HttpEntity<ServiceRequest> httpEntity=new HttpEntity<ServiceRequest>(serviceRequest,headers);
-		  String finalUrl =userHost+userCreateEndPoint;
-		  //System.out.println("Final url  .... ....... "+finalUrl);
+		  serviceResponse = restTemplate.postForObject(uri.toString(), serviceRequest, ServiceResponse.class);
 		  
-		  ResponseEntity<ServiceRequest>  response1 = restTemplate.postForEntity(userHost+userCreateEndPoint, httpEntity, ServiceRequest.class);
-		  
-		  System.out.println("response   ...  :"+ serviceRequest);
 	  }catch(HttpClientErrorException e) {
-		  
-	  log.error("External Service threw an Exception: ",e);
-	  
-	 // throw new ServiceCallException(e.getResponseBodyAsString());
-	  }catch(Exception e) {
-	 
-	  log.error("Exception while fetching from searcher: ",e); }
-	  
-	  return response;
-	  
+          log.error("External Service threw an Exception: ",e);
+          throw new ServiceCallException(e.getResponseBodyAsString());
+      }catch(Exception e) {
+          log.error("Exception while fetching from searcher: ",e);
+      }
+
+      return serviceResponse;
 	  }
 
-	public String getUserHost() {
-		return userHost;
-	}
-
-	public void setUserHost(String userHost) {
-		this.userHost = userHost;
-	}
-
-	public String getUserCreateEndPoint() {
-		return userCreateEndPoint;
-	}
-
-	public void setUserCreateEndPoint(String userCreateEndPoint) {
-		this.userCreateEndPoint = userCreateEndPoint;
-	}
-
-	public RestTemplate getRestTemplate() {
-		return restTemplate;
-	}
-
-	public void setRestTemplate(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
-	}
-	  
+	
 	  
 	
 }
