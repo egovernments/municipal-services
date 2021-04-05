ce72a5baab2505dbcea4c922978db461168af76e
mahammadnizam-wt-eGov <mahammad.nizam@egovernments.org>
2021-04-05 12:52:58 +0530

    Generating demand only for fazilka

--- ws-calculator/src/main/java/org/egov/wscalculation/service/WSCalculationServiceImpl.java
+++ ws-calculator/src/main/java/org/egov/wscalculation/service/WSCalculationServiceImpl.java
@@ -301,9 +301,9 @@ public class WSCalculationServiceImpl implements WSCalculationService {
 		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		LocalDateTime date = LocalDateTime.now();
 		log.info("Time schedule start for water demand generation on : " + date.format(dateTimeFormatter));
-		List<String> tenantIds = wSCalculationDao.getTenantId();
-//		List<String> tenantIds = new ArrayList<>();
-//		tenantIds.add("pb.fazilka");
+//		List<String> tenantIds = wSCalculationDao.getTenantId();
+		List<String> tenantIds = new ArrayList<>();
+		tenantIds.add("pb.fazilka");
 		if (tenantIds.isEmpty()) {
 			log.info("No tenants are found for generating demand");
 			return;
