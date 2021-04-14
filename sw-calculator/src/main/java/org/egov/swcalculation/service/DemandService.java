5c27c0c1220bf4aac9527d7a75a00b0502789bcf
mahammadnizam-wt-eGov <mahammad.nizam@egovernments.org>
2021-04-14 10:51:01 +0530

    Removed return status after getting exception for single consumer:

--- sw-calculator/src/main/java/org/egov/swcalculation/service/DemandService.java
+++ sw-calculator/src/main/java/org/egov/swcalculation/service/DemandService.java
@@ -893,7 +893,6 @@ public class DemandService {
 			} catch (Exception ex) {
 				log.error("Fetch Bill Error For tenantId:{} consumercode: {} and Exception is: {}",tenantId,consumerCodes, ex);
 				ex.printStackTrace();
-				return Boolean.FALSE;
 			}
 		}
 		return Boolean.TRUE;
