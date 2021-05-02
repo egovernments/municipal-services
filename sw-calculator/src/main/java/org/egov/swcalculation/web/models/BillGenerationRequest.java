e56621f6efa017ab3f2f26e705d19f64b8020315
mohammadrafeek-eGov <mohammad.rafeek@egovernments.org>
2021-05-02 08:46:04 +0530

    PI-1322:Corrected json property name.

--- sw-calculator/src/main/java/org/egov/swcalculation/web/models/BillGenerationRequest.java
+++ sw-calculator/src/main/java/org/egov/swcalculation/web/models/BillGenerationRequest.java
@@ -26,7 +26,7 @@ public class BillGenerationRequest {
 	@NotNull
 	private RequestInfo requestInfo;
 
-	@JsonProperty("BillScheduler")
+	@JsonProperty("billScheduler")
 	@Valid
 	private BillScheduler billScheduler;
 
