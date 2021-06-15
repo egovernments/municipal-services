11637b83aa4c096e520ecbc469d49dd38ed6214a
KarunaNidhan-egov <karuna.nidhan@egovernments.org>
2021-06-15 12:17:05 +0530

    PI-1586

--- ws-services/src/main/java/org/egov/waterconnection/service/WaterServiceImpl.java
+++ ws-services/src/main/java/org/egov/waterconnection/service/WaterServiceImpl.java
@@ -167,11 +167,12 @@ public class WaterServiceImpl implements WaterService {
 
 	List<WaterConnection> getWaterConnectionPlainSearch(SearchCriteria criteria, RequestInfo requestInfo) {
 		
-		if(criteria.getLimit()==null)
+		if(criteria.getLimit()==null) {
 			criteria.setLimit(config.getDefaultLimit());
-		
-		if (criteria.getLimit() != null && criteria.getLimit() > config.getMaxLimit())
+		}
+		else if (criteria.getLimit() != null && criteria.getLimit() > config.getMaxLimit()) {
 			criteria.setLimit(config.getMaxLimit());				
+		}
 		
 		if(criteria.getOffset()==null)
 			criteria.setOffset(config.getDefaultOffset());
