56c743acf46c853ffc8d7a98672ab18b62fcf187
Deepamulimani-eGov <deepa.mulimani@egovernments.org>
2021-06-16 13:03:04 +0530

    FSM-461 - Plant mapping (#849)
    * FSM-461: FSTP plant mapping to FSTP employee code.
    
    * FSM-461: On trip disposed,capture plant code in vehicle trip
    
    * FSM-461: fstp mapping table sql script
    
    * [FSM-461]: Save plant mapping
    
    * [FMS-461] : Update plant mapping fixes
    
    * [FSM-461]: For FSM business service update plancode details
    
    * [FSM-461]: Code review fixes
    
    * [FSM-461]: Code review fixes
    
    * [FSM-461: Code review changes
    
    Co-authored-by: lakshmi-sravani <lakshmisravani.kolla@walkingtree.tech>

--- fsm/src/main/java/org/egov/fsm/validator/MDMSValidator.java
+++ fsm/src/main/java/org/egov/fsm/validator/MDMSValidator.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.egov.fsm.plantmapping.util.PlantMappingConstants;
+import org.egov.fsm.plantmapping.web.model.PlantMappingRequest;
 import org.egov.fsm.util.FSMConstants;
 import org.egov.fsm.util.FSMErrorConstants;
 import org.egov.fsm.web.model.FSMRequest;
@@ -131,6 +133,32 @@ public class MDMSValidator {
 		if (!errorMap.isEmpty())
 			throw new CustomException(errorMap);
 		
+	}
+	public void validateMdmsData(PlantMappingRequest request, Object mdmsData) {
+
+		this.mdmsResMap = getAttributeValues(mdmsData);
+		String[] masterArray = { PlantMappingConstants.MDMS_FSTP_PLANT_INFO };
+
+		validateIfMasterPresent(masterArray, this.mdmsResMap);
+
+	}
+
+	public void validateFSTPPlantInfo(String plantCode, String tenantId) throws CustomException {
+		Map<String, String> errorMap = new HashMap<>();
+		List<Map<String, String>> plantMap = (List<Map<String, String>>) this.mdmsResMap
+				.get(PlantMappingConstants.MDMS_FSTP_PLANT_INFO);
+		List<Map<String, String>> fstpmap = (List<Map<String, String>>) JsonPath.parse(plantMap)
+				.read("$.[?(@.PlantCode=='" + plantCode + "')]");
+		if (fstpmap != null && fstpmap.size() > 0) {
+			Map<String, String> planMapData = fstpmap.get(0);
+			if (!(planMapData.get("PlantCode").equals(plantCode)) || !(planMapData.get("ULBS").contains(tenantId))) {
+				errorMap.put(FSMErrorConstants.INVALID_FSTP_CODE, "Invalid FSTP code");
+			}
+		} else {
+			errorMap.put(FSMErrorConstants.INVALID_FSTP_CODE, "Invalid FSTP code");
+		}
+		if (!errorMap.isEmpty())
+			throw new CustomException(errorMap);
 	}
 
 }
