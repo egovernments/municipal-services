bc04ef3b2f347a319af43d2b1a7245fb386b3ce1
KarunaNidhan-egov <karuna.nidhan@egovernments.org>
2021-06-24 19:21:22 +0530

    PI-1582: pagination modification

--- ws-services/src/main/java/org/egov/waterconnection/repository/builder/WsQueryBuilder.java
+++ ws-services/src/main/java/org/egov/waterconnection/repository/builder/WsQueryBuilder.java
@@ -241,8 +241,8 @@ public class WsQueryBuilder {
 	private String addPaginationWrapper(String query, List<Object> preparedStmtList, SearchCriteria criteria) {
 		Integer limit = config.getDefaultLimit();
 		Integer offset = config.getDefaultOffset();
-//		if (criteria.getLimit() == null && criteria.getOffset() == null)
-//			limit = config.getMaxLimit();
+		if (criteria.getLimit() == null && criteria.getOffset() == null)
+			limit = config.getMaxLimit();
 //
 //		if (criteria.getLimit() != null && criteria.getLimit() <= config.getDefaultLimit())
 //			limit = criteria.getLimit();
