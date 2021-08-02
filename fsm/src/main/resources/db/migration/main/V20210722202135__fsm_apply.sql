3d40b56ea63233ff501dad5671e9ab48bce28951
narendrabandhamneni-wt-egov <narendra.bandhamaneni@egovernments.org>
2021-08-02 17:15:42 +0530

    corrected code

--- fsm/src/main/resources/db/migration/main/V20210722202135__fsm_apply.sql
+++ fsm/src/main/resources/db/migration/main/V20210722202135__fsm_apply.sql
@@ -1,6 +1,6 @@
-ALTER TABLE eg_fsm_application ADD COLUMN applicationtype character varying(256) DEFAULT 'Adhoc Service' NOT NULL;
+ALTER TABLE eg_fsm_application ADD COLUMN applicationtype character varying(256) DEFAULT 'ADHOC' NOT NULL;
 
-ALTER TABLE eg_fsm_application_auditlog ADD COLUMN applicationtype character varying(256) DEFAULT 'Adhoc Service' NOT NULL;
+ALTER TABLE eg_fsm_application_auditlog ADD COLUMN applicationtype character varying(256) DEFAULT 'ADHOC' NOT NULL;
 	
 ALTER TABLE eg_fsm_application ADD COLUMN oldapplicationno character varying(256) DEFAULT NULL;
 
