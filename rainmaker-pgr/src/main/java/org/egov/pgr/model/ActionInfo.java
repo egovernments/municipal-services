92afed6345f32c57729f3e824610501fabd1abc8
egovmis <42794267+egovmis@users.noreply.github.com>
2021-09-03 11:02:32 +0530

    added - _ + in comments

--- rainmaker-pgr/src/main/java/org/egov/pgr/model/ActionInfo.java
+++ rainmaker-pgr/src/main/java/org/egov/pgr/model/ActionInfo.java
@@ -62,7 +62,7 @@ public class ActionInfo   {
   private List<String> media = null;
 
   @JsonProperty("comments")
-  @Pattern(regexp = "^[a-zA-Z0-9!@#.,/:; ()&']*$")
+  @Pattern(regexp = "^[a-zA-Z0-9!@#.,/:; _-+=%()&']*$")
   @Size(max=256)
   private String comment = null;
 
