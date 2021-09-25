76ee2b2b593fddb98c85a2ab5e7fb831efe1bcee
gurpreetsinghkhanna <45963460+gurpreetsinghkhanna@users.noreply.github.com>
2021-09-25 14:17:56 +0530

    representable decimal result fix

--- pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/EstimationService.java
+++ pt-calculator-v2/src/main/java/org/egov/pt/calculator/service/EstimationService.java
@@ -543,7 +543,7 @@ public class EstimationService {
 		PropertyDetail detail = property.getPropertyDetails().get(0);
 		BigDecimal payableTax = taxAmt.setScale(2, 2);
 		// To find Percentage Share of Commecial Tax ( to be used for firecess) after subtracting exemptions) = commercialTax/TaxPayable*100
-		BigDecimal commercialTaxAmtRatioPercent=commercialTaxAmt.divide(payableTax).multiply(new BigDecimal(100.00)).setScale(2,BigDecimal.ROUND_HALF_UP);
+		BigDecimal commercialTaxAmtRatioPercent=commercialTaxAmt.divide(payableTax,2,BigDecimal.ROUND_HALF_UP).multiply(new BigDecimal(100.00)).setScale(2,BigDecimal.ROUND_HALF_UP);
 		
 		
 		List<TaxHeadEstimate> estimates = new ArrayList<>();
