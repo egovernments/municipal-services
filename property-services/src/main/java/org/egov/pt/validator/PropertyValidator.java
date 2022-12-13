973d83e03591d2cdd08dc94414083645e747f044
shiva-eGov <shiva.prasad@egovernments.org>
2022-12-13 15:12:17 +0530

    changed the customizes exception.

--- property-services/src/main/java/org/egov/pt/validator/PropertyValidator.java
+++ property-services/src/main/java/org/egov/pt/validator/PropertyValidator.java
@@ -250,7 +250,7 @@ public static final Logger log = LoggerFactory.getLogger(PropertyValidator.class
 					"The Creation reason sent in the update Request is Invalid, The Creationg reason can be changed only when a new process is initiated on an ACTIVE record");
 		} else if (propertyFromSearch.getStatus().equals(Status.ACTIVE) && reason.equals(CreationReason.CREATE)) {
 			throw new CustomException("EG_PT_ERROR_CREATION_REASON",
-					"The Creation reason sent in the update Request is Invalid, The Creationg reason cannot be create for an ACTIVE record");
+					"There is something wrong! The status of the property record cannot be ACTIVE on create.");
 		}
 
 		property.getAddress().setId(propertiesFromSearchResponse.get(0).getAddress().getId());
